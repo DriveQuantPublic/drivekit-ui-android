5f0ff107e60a37b1ccefe306f88d8a14734408e0
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-02-13 17:13:14 +0100

    Remove optional from method signature

--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/utils/DurationUtils.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/utils/DurationUtils.kt
@@ -4,11 +4,8 @@ import android.content.Context
 import com.drivequant.drivekit.driverachievement.ui.R
 
 class DurationUtils {
-    fun formatDuration(context: Context, duration: Double?): String {
-        var formattedDuration = ""
-        var computedDuration: Double? = duration
-
-        if (computedDuration != null) {
+    fun formatDuration(context: Context, duration: Double): String {
+        var computedDuration: Double = duration
             computedDuration = if (computedDuration % 60 > 0) {
                 (computedDuration.toInt() + 60).toDouble()
             } else {
@@ -16,23 +13,21 @@ class DurationUtils {
             }
 
             val durationInt: Int = (computedDuration / 60).toInt()
-            formattedDuration = if (durationInt > 60) {
-                val minDuration: String =
-                    if (durationInt % 60 < 10) "0" + durationInt % 60 else (durationInt % 60).toString()
-                String.format(
-                    "%d%s%s",
-                    durationInt / 60,
-                    context.resources.getString(R.string.dk_unit_hour),
-                    minDuration
-                )
-            } else {
-                String.format(
-                    "%d %s",
-                    durationInt,
-                    context.resources.getString(R.string.dk_unit_minute)
-                )
-            }
+        return if (durationInt > 60) {
+            val minDuration: String =
+                if (durationInt % 60 < 10) "0" + durationInt % 60 else (durationInt % 60).toString()
+            String.format(
+                "%d%s%s",
+                durationInt / 60,
+                context.resources.getString(R.string.dk_unit_hour),
+                minDuration
+            )
+        } else {
+            String.format(
+                "%d %s",
+                durationInt,
+                context.resources.getString(R.string.dk_unit_minute)
+            )
         }
-        return formattedDuration
     }
 }
