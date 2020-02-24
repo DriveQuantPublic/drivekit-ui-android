8efcce0b5324881ec4886c3a63cc46c0c5dbb81f
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-02-24 18:14:23 +0100

    Add resources

---
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/DKConstants.kt
@@ -0,0 +1,13 @@
+package com.drivequant.drivekit.common.ui
+
+object DKConstants {
+    const val FORMAT_WEEK_LETTER = "EEEE d MMMM"
+    const val FORMAT_STANDARD_DATE = "dd/MM/yyyy"
+    const val FORMAT_HOUR_MINUT = "HH'h'mm"
+    const val FORMAT_FULL_DATE = "EEEE d MMMM yyyy"
+    const val FORMAT_DAY_MONTH = "dd/MM"
+    const val FORMAT_YEAR_ONLY = "E"
+    const val FORMAT_YEAR = "yyyy"
+    const val FORMAT_DAY = "EEEE"
+    const val FORMAT_MONTH = "M"
+}
