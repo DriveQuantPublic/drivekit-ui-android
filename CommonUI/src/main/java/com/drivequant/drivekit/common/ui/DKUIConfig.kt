f28f8273cf8060946a3a0bafb396d9ada0a7de21
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-02-26 16:27:10 +0100

    Clean code

--- CommonUI/src/main/java/com/drivequant/drivekit/common/ui/DKUIConfig.kt
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/DKUIConfig.kt
@@ -2,7 +2,6 @@ package com.drivequant.drivekit.common.ui
 
 import com.drivequant.drivekit.common.ui.utils.DistanceUnit
 
-
 object DKUIConfig {
     var distanceUnit: DistanceUnit = DistanceUnit.KM
 }
