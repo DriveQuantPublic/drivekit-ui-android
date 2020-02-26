3a0cb7d62f0a9852d0c687c45bca05971702e7a9
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-02-26 15:24:17 +0100

    Add gauge view with some changes

---
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/component/GaugeIndicator.kt
@@ -0,0 +1,117 @@
+package com.drivequant.drivekit.common.ui.component
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
+
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.widget.ImageView
+import android.widget.TextView
+import android.util.Log
+import com.drivequant.drivekit.common.ui.DKFonts
+import com.drivequant.drivekit.common.ui.DriveKitUI
+import com.drivequant.drivekit.common.ui.R
+import com.drivequant.drivekit.common.ui.extension.removeZeroDecimal
+import java.text.DecimalFormat
+
+class GaugeIndicator(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private val textView: TextView
+    private val gaugeView: GaugeView
+    private val imageView: ImageView
+
+    init {
+        inflate(context, R.layout.layout_gauge_view, this)
+
+        textView = findViewById(R.id.score)
+        gaugeView = findViewById(R.id.gauge)
+        imageView = findViewById(R.id.image)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.GaugeIndicator)
+
+        imageView.setImageDrawable(attributes.getDrawable(R.styleable.GaugeIndicator_image))
+
+        gaugeView.updateStrokeSize(
+            attributes.getDimension(
+                R.styleable.GaugeIndicator_strokeSize,
+                resources.getDimension(R.dimen.dk_default_gauge_stroke)
+            )
+        )
+
+        gaugeView.setBackGaugeColor(
+            attributes.getColor(
+                R.styleable.GaugeIndicator_backColor,
+                ContextCompat.getColor(context, R.color.dkMainFontColor)
+            )
+        )
+
+        textView.setTextSize(
+            TypedValue.COMPLEX_UNIT_SP,
+            attributes.getDimensionPixelSize(
+                R.styleable.GaugeIndicator_textSize,
+                resources.getDimensionPixelSize(R.dimen.dk_text_normal)
+            ).toFloat()
+        )
+        attributes.recycle()
+    }
+
+    fun configure(score: Double, type: GaugeType) {
+        textView.text = score.removeZeroDecimal()
+        textView.typeface = DriveKitUI.primaryFont(context)
+        gaugeView.configureScore(score)
+        gaugeView.setGaugeColor(ContextCompat.getColor(context, type.getColor(score)))
+        imageView.setImageDrawable(ContextCompat.getDrawable(context, type.getDrawable()))
+    }
+}
+
+enum class GaugeType {
+    SAFETY, ECO_DRIVING, DISTRACTION;
+
+    fun getColor(score: Double): Int {
+        return getColorFromValue(score, getSteps())
+    }
+
+    fun getDrawable(): Int {
+        return when (this) {
+            ECO_DRIVING -> R.drawable.dk_common_ecodriving
+            SAFETY -> R.drawable.dk_common_safety
+            DISTRACTION -> R.drawable.dk_common_distraction
+        }
+    }
+
+    private fun getColorFromValue(value: Double, steps: List<Double>): Int {
+        if (value <= steps[0])
+            return R.color.dkVeryBad
+        if (value <= steps[1])
+            return R.color.dkBad
+        if (value <= steps[2])
+            return R.color.dkBadMean
+        if (value <= steps[3])
+            return R.color.dkMean
+        if (value <= steps[4])
+            return R.color.dkGoodMean
+        return if (value <= steps[5]) R.color.dkGood else R.color.dkExcellent
+    }
+
+    private fun getSteps(): List<Double> {
+        return when (this) {
+            ECO_DRIVING -> {
+                val mean = 7.63
+                val sigma = 0.844
+                listOf(
+                    mean - (2 * sigma),
+                    mean - sigma,
+                    mean - (0.25 * sigma),
+                    mean,
+                    mean + (0.25 * sigma),
+                    mean + sigma,
+                    mean + (2 * sigma)
+                )
+            }
+            SAFETY -> listOf(0.0, 5.5, 6.5, 7.5, 8.5, 9.5, 10.0)
+            DISTRACTION -> listOf(1.0, 7.0, 8.0, 8.5, 9.0, 9.5, 10.0)
+        }
+    }
+}
---
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/component/GaugeView.kt
@@ -0,0 +1,75 @@
+package com.drivequant.drivekit.common.ui.component
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class GaugeView(context: Context,attrs: AttributeSet) : View(context, attrs) {
+
+    private var drawingArea : RectF = RectF()
+    private var score : Double = 0.0
+    private var strokeSize = 0F
+    private var gaugeColor =  Color.argb(0,0,0,0)
+    private var backGaugeColor =  Color.argb(0,0,0,0)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        calculateDrawableArea()
+        canvas?.drawArc(drawingArea, 270F, 128F, false, createPaint(Color.argb(0,0,0,0)))
+        canvas?.drawArc(drawingArea, 38F, 232F, false, createPaint(backGaugeColor))
+        canvas?.drawArc(drawingArea, 38F, computePercent(), false, createPaint(gaugeColor))
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        calculateDrawableArea()
+    }
+
+    fun configureScore(score: Double){
+        this.score = score
+        invalidate()
+    }
+
+    fun setGaugeColor(color: Int){
+        this.gaugeColor = color
+        invalidate()
+    }
+
+    fun setBackGaugeColor(color: Int){
+        this.backGaugeColor = color
+        invalidate()
+    }
+
+    fun updateStrokeSize(strokeSize: Float){
+        this.strokeSize = strokeSize
+        calculateDrawableArea()
+    }
+
+    private fun calculateDrawableArea() {
+        val drawPadding = strokeSize / 2
+        val width = width.toFloat()
+        val height = height.toFloat()
+        val right = width - drawPadding
+        val bottom = height - drawPadding
+        drawingArea = RectF(drawPadding, drawPadding, right, bottom)
+    }
+
+
+    private fun computePercent() : Float{
+        return (232F * score / 10F).toFloat()
+    }
+
+    private fun createPaint(color : Int) : Paint{
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.color = color
+        paint.style = Paint.Style.STROKE
+        paint.strokeCap = Paint.Cap.ROUND
+        paint.strokeWidth = strokeSize
+        return paint
+    }
+}
