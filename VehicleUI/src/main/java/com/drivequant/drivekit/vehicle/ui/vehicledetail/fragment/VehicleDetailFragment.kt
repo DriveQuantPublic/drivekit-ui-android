6fe27dfb7278a19f2f8be2a554a865ad1de5745d
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2020-03-13 17:08:56 +0100

    Introduce CamerPictureGallery helper, camera working

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicledetail/fragment/VehicleDetailFragment.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicledetail/fragment/VehicleDetailFragment.kt
@@ -1,8 +1,14 @@
 package com.drivequant.drivekit.vehicle.ui.vehicledetail.fragment
 
+import android.Manifest
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +19,14 @@ import com.drivequant.drivekit.common.ui.extension.normalText
 import com.drivequant.drivekit.common.ui.utils.DKAlertDialog
 import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.common.ui.utils.FontUtils
+import com.drivequant.drivekit.core.DriveKitSharedPreferencesUtils
 import com.drivequant.drivekit.vehicle.ui.R
 import com.drivequant.drivekit.vehicle.ui.extension.computeTitle
+import com.drivequant.drivekit.vehicle.ui.listener.OnCameraPictureTakenCallback
+import com.drivequant.drivekit.vehicle.ui.vehicledetail.common.CameraGalleryPickerHelper
 import com.drivequant.drivekit.vehicle.ui.vehicledetail.viewmodel.VehicleDetailViewModel
 import kotlinx.android.synthetic.main.fragment_vehicle_detail.*
+import java.util.prefs.PreferenceChangeEvent
 
 class VehicleDetailFragment : Fragment() {
 
@@ -30,6 +40,9 @@ class VehicleDetailFragment : Fragment() {
 
     private lateinit var viewModel : VehicleDetailViewModel
     private lateinit var vehicleId : String
+    private var cameraFilePath : String? = null
+
+    private lateinit var onCameraCallback: OnCameraPictureTakenCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,7 +68,12 @@ class VehicleDetailFragment : Fragment() {
         viewModel = ViewModelProviders.of(this,
             VehicleDetailViewModel.VehicleDetailViewModelFactory(vehicleId)
         ).get(VehicleDetailViewModel::class.java)
-
+        cameraFilePath = DriveKitSharedPreferencesUtils.getString("VEHICLE_PICTURE_PREF_%s") // TODO: %s = vehicleId
+        onCameraCallback = object : OnCameraPictureTakenCallback {
+            override fun pictureTaken(filePath: String) {
+                cameraFilePath = filePath
+            }
+        }
 
         collapsing_toolbar.title = viewModel.vehicle?.computeTitle(requireContext(), listOf()) // TODO: create a method to retrieve all vehicles, everywhere
         collapsing_toolbar.setExpandedTitleColor(DriveKitUI.colors.fontColorOnPrimaryColor())
@@ -93,9 +111,32 @@ class VehicleDetailFragment : Fragment() {
         separatorCamera?.setBackgroundColor(neutralColor)
         separatorGallery?.setBackgroundColor(neutralColor)
         cameraTextView?.let {
-            it.text = DKResource.convertToString(requireContext(), "dk_take_picture")
+            it.text = DKResource.convertToString(requireActivity(), "dk_take_picture")
             it.setOnClickListener {
-                // TODO launch camera
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                            Manifest.permission.CAMERA)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(requireActivity(),
+                            arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 12)
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    // Permission has already been granted
+                    launchCameraIntent()
+                }
             }
         }
         galleryTextView?.let {
@@ -104,5 +145,17 @@ class VehicleDetailFragment : Fragment() {
                 // TODO launch gallery
             }
         }
+    }
+
+    private fun launchCameraIntent(){
+        viewModel.vehicle?.let {
+            CameraGalleryPickerHelper.openCamera(requireActivity(), it.vehicleId, onCameraCallback)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 }
