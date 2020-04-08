411298f8dbb3672bcbd4f5b54e8152bdee1cfcc2
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-04-08 10:14:15 +0200

    Create permission views  navigation engine

--- PermissionsUtilsUI/src/main/java/com/drivequant/drivekit/permissionsutils/permissions/PermissionView.kt
+++ PermissionsUtilsUI/src/main/java/com/drivequant/drivekit/permissionsutils/permissions/PermissionView.kt
@@ -1,6 +1,12 @@
 package com.drivequant.drivekit.permissionsutils.permissions
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import com.drivequant.drivekit.permissionsutils.PermissionUtilsUI
+import com.drivequant.drivekit.permissionsutils.diagnosis.DiagnosisHelper
+import com.drivequant.drivekit.permissionsutils.permissions.BasePermissionActivity.Companion.PERMISSION_VIEWS_LIST_EXTRA
 
 /**
  * Created by Mohamed on 2020-04-02.
@@ -8,9 +14,37 @@ import android.content.Context
 // Copyright (c) 2020 DriveQuant. All rights reserved.
 
 enum class PermissionView {
-    ACTIVITY, LOCATION, BATTERY;
+    ACTIVITY, LOCATION, BACKGROUND_TASK;
 
-    fun launchActivity(context: Context, permissionViews:ArrayList<PermissionView>) {
+    fun launchActivity(activity: Activity, permissionViews: ArrayList<PermissionView>) {
+        if (!this.isAuthorized(activity)) {
+            activity.startActivity(this.buildIntent(activity, permissionViews))
+        } else {
+            permissionViews.remove(this)
+            if (permissionViews.isEmpty()) {
+                PermissionUtilsUI.permissionViewListener?.onFinish()
+            } else {
+                permissionViews.first().launchActivity(activity, permissionViews)
+            }
+        }
+    }
 
+    private fun isAuthorized(context: Activity): Boolean {
+        return when (this) {
+            LOCATION -> DiagnosisHelper.isLocationAuthorize(context)
+            ACTIVITY -> DiagnosisHelper.isActivityRecognitionAuthorize(context)
+            BACKGROUND_TASK -> DiagnosisHelper.isIgnoringBatteryOptimizations(context)
+        }
+    }
+
+    private fun buildIntent(context: Context, permissionViews: ArrayList<PermissionView>): Intent {
+        val intent = when (this) {
+            LOCATION -> Intent(context, LocationPermissionActivity::class.java)
+            ACTIVITY -> Intent(context, ActivityRecognitionPermissionActivity::class.java)
+            BACKGROUND_TASK -> Intent(context, BackgroundTaskPermissionActivity::class.java)
+        }
+        intent.putExtra(PERMISSION_VIEWS_LIST_EXTRA, permissionViews)
+        intent.flags = FLAG_ACTIVITY_NEW_TASK
+        return intent
     }
 }
