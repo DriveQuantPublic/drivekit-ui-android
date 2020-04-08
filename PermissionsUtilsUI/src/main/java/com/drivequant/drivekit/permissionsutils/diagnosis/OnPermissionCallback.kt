94701b966d2c058ca245011616bfd86f4ae9d195
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-04-08 10:23:24 +0200

    Request Activity permission

---
+++ PermissionsUtilsUI/src/main/java/com/drivequant/drivekit/permissionsutils/diagnosis/OnPermissionCallback.kt
@@ -0,0 +1,12 @@
+package com.drivequant.drivekit.permissionsutils.diagnosis
+
+/**
+ * Created by Mohamed on 2020-04-03.
+ */
+// Copyright (c) 2020 DriveQuant. All rights reserved.
+
+interface OnPermissionCallback {
+    fun onPermissionGranted(permissionName: Array<String>)
+    fun onPermissionDeclined(permissionName: Array<String>)
+    fun onPermissionTotallyDeclined(permissionName: String)
+}
--- PermissionsUtilsUI/src/main/java/com/drivequant/drivekit/permissionsutils/permissions/ActivityRecognitionPermissionActivity.kt
+++ PermissionsUtilsUI/src/main/java/com/drivequant/drivekit/permissionsutils/permissions/ActivityRecognitionPermissionActivity.kt
@@ -1,29 +1,81 @@
 package com.drivequant.drivekit.permissionsutils.permissions
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.drivequant.drivekit.common.ui.extension.button
 import com.drivequant.drivekit.common.ui.extension.headLine1
 import com.drivequant.drivekit.common.ui.extension.normalText
+import com.drivequant.drivekit.common.ui.utils.DKAlertDialog
 import com.drivequant.drivekit.permissionsutils.R
+import com.drivequant.drivekit.permissionsutils.diagnosis.DiagnosisHelper.PERMISSION_ACTIVITY_RECOGNITION
+import com.drivequant.drivekit.permissionsutils.diagnosis.DiagnosisHelper.REQUEST_PERMISSIONS_OPEN_SETTINGS
+import com.drivequant.drivekit.permissionsutils.diagnosis.OnPermissionCallback
 
 class ActivityRecognitionPermissionActivity : BasePermissionActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recognition_permission)
+        findViewById<TextView>(R.id.text_view_activity_permission_title).headLine1()
+        findViewById<TextView>(R.id.text_view_activity_permission_text).normalText()
+        findViewById<Button>(R.id.button_request_activity_permission).button()
+    }
 
-        val textViewActivityPermissionTitle = findViewById<TextView>(R.id.text_view_activity_permission_title)
-        val textViewActivityPermissionText = findViewById<TextView>(R.id.text_view_activity_permission_text)
-        val buttonActivityPermissionRequest = findViewById<Button>(R.id.button_request_activity_permission)
+    fun onRequestPermissionClicked(view: View) {
+        checkRequiredPermissions()
+    }
 
-        textViewActivityPermissionTitle.headLine1()
-        textViewActivityPermissionText.normalText()
-        buttonActivityPermissionRequest.button()
+    private fun checkRequiredPermissions() {
+        permissionCallback = object : OnPermissionCallback {
+            override fun onPermissionGranted(permissionName: Array<String>) {
+                next()
+                finish()
+            }
 
-        buttonActivityPermissionRequest.setOnClickListener {
-            next()
+            override fun onPermissionDeclined(permissionName: Array<String>) {
+                DKAlertDialog.AlertBuilder()
+                    .init(this@ActivityRecognitionPermissionActivity)
+                    .title(getString(R.string.dk_perm_utils_permissions))
+                    .message(getString(R.string.dk_perm_utils_app_diag_activity_ko))
+                    .cancelable(false)
+                    .positiveButton(getString(R.string.dk_common_ok),
+                        DialogInterface.OnClickListener { _, _ ->
+                            checkRequiredPermissions()
+                        })
+                    .show()
+            }
+
+            override fun onPermissionTotallyDeclined(permissionName: String) {
+                DKAlertDialog.AlertBuilder()
+                    .init(this@ActivityRecognitionPermissionActivity)
+                    .title(getString(R.string.dk_perm_utils_permissions))
+                    .message(getString(R.string.dk_perm_utils_app_diag_activity_ko))
+                    .cancelable(false)
+                    .positiveButton(getString(R.string.dk_perm_utils_settings),
+                        DialogInterface.OnClickListener { _, _ ->
+                            startActivityForResult(
+                                launchSettings(),
+                                REQUEST_PERMISSIONS_OPEN_SETTINGS
+                            )
+                        })
+                    .negativeButton(getString(R.string.dk_common_close),
+                        DialogInterface.OnClickListener { dialog, _ ->
+                            dialog.cancel()
+                        })
+                    .show()
+            }
+        }
+        request(this, permissionCallback as OnPermissionCallback, PERMISSION_ACTIVITY_RECOGNITION)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_PERMISSIONS_OPEN_SETTINGS) {
+            checkRequiredPermissions()
         }
     }
 }
