3a92f59cb637e73cf842b7a16898d63f7af5e970
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-05-11 17:04:33 +0200

    Add new config parameters in Achievement entry point

--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/DriverAchievementUI.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/DriverAchievementUI.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.support.v4.app.Fragment
 import com.drivequant.drivekit.common.ui.navigation.DriveKitNavigationController
 import com.drivequant.drivekit.common.ui.navigation.DriverAchievementUIEntryPoint
+import com.drivequant.drivekit.databaseutils.entity.BadgeCategory
 import com.drivequant.drivekit.databaseutils.entity.StreakTheme
 import com.drivequant.drivekit.driverachievement.ui.streaks.activity.StreaksListActivity
 import com.drivequant.drivekit.driverachievement.ui.streaks.fragment.StreaksListFragment
@@ -18,12 +19,23 @@ object DriverAchievementUI : DriverAchievementUIEntryPoint {
         StreakTheme.BRAKE,
         StreakTheme.ADHERENCE)
 
+    internal var badgeCategories: List<BadgeCategory> = listOf(
+        BadgeCategory.PHONE_DISTRACTION,
+        BadgeCategory.SAFETY,
+        BadgeCategory.ECO_DRIVING,
+        BadgeCategory.GENERIC
+    )
+
     fun initialize() {
         DriveKitNavigationController.driverAchievementUIEntryPoint = this
     }
 
     fun configureStreakThemes(streakThemes: List<StreakTheme>) {
         this.streakThemes = streakThemes
+    }
+
+    fun configureBadgeCategories(badgeCategories: List<BadgeCategory>) {
+        this.badgeCategories = badgeCategories
     }
 
     override fun startStreakListActivity(context: Context) {
