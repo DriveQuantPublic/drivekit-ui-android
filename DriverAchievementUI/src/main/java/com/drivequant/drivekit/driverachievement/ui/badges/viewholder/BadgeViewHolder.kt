20d6740e8f2b0b4850c26c9de46fcc2ad517cf1c
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-05-12 19:03:56 +0200

    Format and display badges data

--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/viewholder/BadgeViewHolder.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/viewholder/BadgeViewHolder.kt
@@ -6,7 +6,6 @@ import android.widget.TextView
 import com.drivequant.drivekit.common.ui.DriveKitUI
 import com.drivequant.drivekit.common.ui.component.GaugeImage
 import com.drivequant.drivekit.common.ui.extension.headLine2
-import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.driverachievement.ui.R
 import com.drivequant.drivekit.driverachievement.ui.badges.viewmodel.BadgesData
 
@@ -18,30 +17,56 @@ import com.drivequant.drivekit.driverachievement.ui.badges.viewmodel.BadgesData
 class BadgeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val context = itemView.context
     private val title = itemView.findViewById<TextView>(R.id.text_view_badge_title)
-    private val badgeBronze = itemView.findViewById<GaugeImage>(R.id.badge_bronze)
-    private val badgeSilver = itemView.findViewById<GaugeImage>(R.id.badge_silver)
-    private val badgeGold = itemView.findViewById<GaugeImage>(R.id.badge_gold)
+    private val bronzeBadge = itemView.findViewById<GaugeImage>(R.id.badge_bronze)
+    private val silverBadge = itemView.findViewById<GaugeImage>(R.id.badge_silver)
+    private val goldBadge = itemView.findViewById<GaugeImage>(R.id.badge_gold)
+
+    private val bronzeBadgeName = itemView.findViewById<TextView>(R.id.text_view_bronze_badge_name)
+    private val silverBadgeName = itemView.findViewById<TextView>(R.id.text_view_silver_badge_name)
+    private val goldBadgeName = itemView.findViewById<TextView>(R.id.text_view_gold_badge_name)
 
     fun bind(badgesData: BadgesData) {
-        title.text = badgesData.getBadgeTitle()
-        //
-        DKResource.convertToDrawable(context, "trip_number_0")?.let {
-            badgeBronze.configure(computePercent(badgesData.getProgress(),badgesData.getThreshold()), it, R.color.dkGood)
+        title.text = badgesData.getBadgeTitle(context)
+
+        badgesData.getIcon(context).first?.let {
+            bronzeBadge.configure(
+                badgesData.getPercent().first,
+                it,
+                badgesData.getBadgeColor(
+                    badgesData.getBadgeLevel1().level,
+                    badgesData.isBadgeAcquired().first
+                )
+            )
+            bronzeBadgeName.text = badgesData.getBadgeName(context).first
         }
 
-        DKResource.convertToDrawable(context, "trip_number_0")?.let {
-            badgeSilver.configure(8.04 , it, R.color.dkGood)
+        badgesData.getIcon(context).second?.let {
+            silverBadge.configure(
+                badgesData.getPercent().second,
+                it,
+                badgesData.getBadgeColor(
+                    badgesData.getBadgeLevel2().level,
+                    badgesData.isBadgeAcquired().second
+                )
+            )
+            silverBadgeName.text = badgesData.getBadgeName(context).second
         }
 
-        DKResource.convertToDrawable(context, "trip_number_0")?.let {
-            badgeGold.configure(5.0, it, R.color.dkGood)
+
+        badgesData.getIcon(context).third?.let {
+            goldBadge.configure(
+                badgesData.getPercent().third,
+                it,
+                badgesData.getBadgeColor(
+                    badgesData.getBadgeLevel3().level,
+                    badgesData.isBadgeAcquired().third
+                )
+            )
+            goldBadgeName.text = badgesData.getBadgeName(context).third
         }
 
         setStyle()
     }
-
-    private fun computePercent(progress: Double, threshold:Int): Double = (progress / threshold) * 10
-
 
     private fun setStyle() {
         title.headLine2()
--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/viewmodel/BadgesData.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/viewmodel/BadgesData.kt
@@ -1,7 +1,12 @@
 package com.drivequant.drivekit.driverachievement.ui.badges.viewmodel
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.databaseutils.entity.BadgeCategory
 import com.drivequant.drivekit.databaseutils.entity.BadgeLevel
+import com.drivequant.drivekit.databaseutils.entity.DKLevel
+import com.drivequant.drivekit.driverachievement.ui.R
 
 /**
  * Created by Mohamed on 2020-05-05.
@@ -14,9 +19,65 @@ class BadgesData(
     private val levels: List<BadgeLevel>
 ) {
 
-    fun getBadgeTitle(): String = themeKey
+    fun getBadgeTitle(context: Context): String = DKResource.convertToString(context, themeKey)
 
-    fun getProgress(): Double = levels.first().progressValue
+    fun getBadgeCategory(): BadgeCategory = category
 
-    fun getThreshold(): Int = levels.first().threshold
+    fun getBadgeLevel1() = levels[0]
+
+    fun getBadgeLevel2() = levels[1]
+
+    fun getBadgeLevel3() = levels[2]
+
+    fun isBadgeAcquired(): Triple<Boolean, Boolean, Boolean> =
+        Triple(
+            getBadgeLevel1().progressValue >= getBadgeLevel1().threshold,
+            getBadgeLevel2().progressValue >= getBadgeLevel2().threshold,
+            getBadgeLevel3().progressValue >= getBadgeLevel3().threshold
+        )
+
+
+    fun computePercent(progress: Double, threshold: Int): Double =
+        (progress / threshold) * 10
+
+    fun getPercent(): Triple<Double, Double, Double> =
+        Triple(
+            computePercent(getBadgeLevel1().progressValue, getBadgeLevel1().threshold),
+            computePercent(getBadgeLevel2().progressValue, getBadgeLevel2().threshold),
+            computePercent(getBadgeLevel3().progressValue, getBadgeLevel3().threshold)
+        )
+
+    fun getIcon(context: Context): Triple<Drawable?, Drawable?, Drawable?> =
+        Triple(
+            DKResource.convertToDrawable(context, getBadgeLevel1().iconKey),
+            DKResource.convertToDrawable(context, getBadgeLevel2().iconKey),
+            DKResource.convertToDrawable(context, getBadgeLevel3().iconKey)
+        )
+
+    fun getBadgeColor(dkLevel: DKLevel, isBadgeAcquired: Boolean): Int {
+        return if (isBadgeAcquired) {
+            R.color.neutral
+        } else {
+            when (dkLevel) {
+                DKLevel.GOLD -> R.color.badgeLevel3Color
+                DKLevel.SILVER -> R.color.badgeLevel2Color
+                DKLevel.BRONZE -> R.color.badgeLevel1Color
+            }
+        }
+    }
+
+    fun getBadgeName(context: Context): Triple<String, String, String> =
+        Triple(
+            DKResource.convertToString(context, getBadgeLevel1().nameKey),
+            DKResource.convertToString(context, getBadgeLevel2().nameKey),
+            DKResource.convertToString(context, getBadgeLevel3().nameKey)
+        )
+
+    fun getBadgeDescription(context: Context): Triple<String, String, String> =
+        Triple(
+            DKResource.convertToString(context, getBadgeLevel1().descriptionKey),
+            DKResource.convertToString(context, getBadgeLevel2().descriptionKey),
+            DKResource.convertToString(context, getBadgeLevel3().descriptionKey)
+        )
 }
+
