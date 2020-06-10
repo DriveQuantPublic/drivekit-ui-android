e582599573da847f227d20481ed29fc616d7f122
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2020-06-10 16:46:15 +0200

    Fix vehicle brands list default value

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/DriveKitVehicleUI.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/DriveKitVehicleUI.kt
@@ -25,7 +25,7 @@ import com.drivequant.drivekit.vehicle.ui.vehicles.viewmodel.VehicleActionItem
 object DriveKitVehicleUI : VehicleUIEntryPoint {
 
     internal var vehicleTypes: List<VehicleType> = listOf(VehicleType.CAR)
-    internal var brands: List<VehicleBrand> = VehicleBrand.getBrands(vehicleTypes.first())
+    internal var brands: List<VehicleBrand> = VehicleBrand.values().asList()
     internal var categoryConfigType: CategoryConfigType = CategoryConfigType.BOTH_CONFIG
     internal var vehicleEngineIndexes: List<VehicleEngineIndex> = VehicleEngineIndex.values().toList()
     internal var brandsWithIcons: Boolean = true
