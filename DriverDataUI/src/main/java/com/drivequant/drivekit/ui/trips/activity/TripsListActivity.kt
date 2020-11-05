59d7438c21ab933790a69231592e7f63379ad224
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-11-05 14:28:54 +0100

    Set toolbar on trip list activity.

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/activity/TripsListActivity.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/activity/TripsListActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.drivequant.drivekit.common.ui.navigation.DriveKitNavigationController
+import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.core.SynchronizationType
 import com.drivequant.drivekit.ui.R
 import com.drivequant.drivekit.ui.tripdetail.activity.TripDetailActivity
@@ -19,6 +21,14 @@ class TripsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trips_list)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        val toolbar = findViewById<Toolbar>(R.id.dk_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        title = DKResource.convertToString(this, "dk_driverdata_trips_list_title")
 
         DriveKitNavigationController.driverDataUIEntryPoint?.let {
             fragment = it.createTripListFragment() as TripsListFragment
@@ -38,5 +48,10 @@ class TripsListActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
