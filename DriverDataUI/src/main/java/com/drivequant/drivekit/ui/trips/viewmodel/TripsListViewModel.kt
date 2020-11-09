95348637cf9011c195e974d27659c7f4ad98b59a
Steven <56969493+Steven-DriveQuant@users.noreply.github.com>
2020-11-09 18:46:36 +0100

    Bugfix/rework vehicle filter items computation (#57)
    * Build "All Trips" filter item inside DriverData instead of Vehicle
    
    * Clean code
    
    * Improve code by removing unnecessary listener and conditions
    
    Co-authored-by: Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/viewmodel/TripsListViewModel.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/viewmodel/TripsListViewModel.kt
@@ -9,7 +9,6 @@ import com.drivequant.drivekit.common.ui.DriveKitUI
 import com.drivequant.drivekit.common.ui.adapter.FilterItem
 import com.drivequant.drivekit.common.ui.extension.resSpans
 import com.drivequant.drivekit.common.ui.navigation.DriveKitNavigationController
-import com.drivequant.drivekit.common.ui.navigation.GetVehiclesFilterItems
 import com.drivequant.drivekit.common.ui.utils.DKDataFormatter
 import com.drivequant.drivekit.common.ui.utils.DKSpannable
 import com.drivequant.drivekit.core.SynchronizationType
@@ -24,7 +23,7 @@ import java.util.*
 
 class TripsListViewModel : ViewModel() {
     var tripsByDate: MutableList<TripsByDate> = mutableListOf()
-    var filterItems: List<FilterItem> = listOf()
+    var filterItems: MutableList<FilterItem> = mutableListOf()
     val tripsData: MutableLiveData<List<TripsByDate>> = MutableLiveData()
     val filterData: MutableLiveData<List<FilterItem>> = MutableLiveData()
     var syncStatus: TripsSyncStatus = TripsSyncStatus.NO_ERROR
@@ -53,7 +52,7 @@ class TripsListViewModel : ViewModel() {
     }
 
     fun filterTrips(dayTripDescendingOrder: Boolean) {
-        val trips = filterItems[currentFilterItemPosition].itemId?.let {
+        val trips = filterItems[currentFilterItemPosition].getItemId()?.let {
             allTrips.filter { it1 -> it1.vehicleId == it as String }
         } ?: kotlin.run {
             allTrips
@@ -79,14 +78,11 @@ class TripsListViewModel : ViewModel() {
     }
 
     fun getVehiclesFilterItems(context: Context) {
-        DriveKitNavigationController.vehicleUIEntryPoint?.getVehiclesFilterItems(
-            context,
-            object : GetVehiclesFilterItems {
-                override fun onFilterItemsReceived(vehiclesFilterItems: List<FilterItem>) {
-                    filterItems = vehiclesFilterItems
-                    filterData.postValue(filterItems)
-                }
-            })
+        DriveKitNavigationController.vehicleUIEntryPoint?.getVehiclesFilterItems(context)?.let {
+            filterItems.add(AllTripsFilterItem())
+            filterItems.addAll(it)
+        }
+        filterData.postValue(filterItems)
     }
 
     fun getTripSynthesisText(context: Context): SpannableString {
@@ -110,6 +106,6 @@ class TripsListViewModel : ViewModel() {
     }
 
     fun getVehicleFilterVisibility(): Boolean {
-        return filterItems.isNotEmpty() && filterItems.size != 2 && filterItems[0].itemId == null
+        return filterItems.size > 1
     }
 }
