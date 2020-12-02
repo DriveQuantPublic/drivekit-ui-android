d4f6c97b504d2337c8a3d364f3a9adcabfcfa252
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2020-12-02 15:16:30 +0100

    Review UI

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/commons/views/TripInfoView.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/commons/views/TripInfoView.kt
@@ -24,16 +24,16 @@ class TripInfoView : LinearLayout {
 
     private fun init(trip: Trip, tripInfo: DKTripInfo) {
         val view = View.inflate(context, R.layout.trip_info_item, null)
-        text_view_trip_info.setTextColor(DriveKitUI.colors.fontColorOnSecondaryColor())
+        view.text_view_trip_info.setTextColor(DriveKitUI.colors.fontColorOnSecondaryColor())
         DrawableCompat.setTint(view.background, DriveKitUI.colors.secondaryColor())
         tripInfo.getImageResource(trip)?.let {
-            image_view_trip_info.setImageResource(it)
+            view.image_view_trip_info.setImageResource(it)
         }
         tripInfo.text(trip)?.let {
-            text_view_trip_info.visibility = View.VISIBLE
-            text_view_trip_info.text = it
+            view.text_view_trip_info.visibility = View.VISIBLE
+            view.text_view_trip_info.text = it
         } ?: run {
-            text_view_trip_info.visibility = View.GONE
+            view.text_view_trip_info.visibility = View.GONE
         }
         view.setOnClickListener {
             if (tripInfo.hasActionConfigured(trip)) {
