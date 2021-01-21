4d8b4bf512bb5c4b1969d92088311413311eb18f
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2021-01-21 10:47:24 +0100

    Fix merge conflicts

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/viewmodel/TripDetailViewModel.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/viewmodel/TripDetailViewModel.kt
@@ -52,13 +52,7 @@ internal class TripDetailViewModel(
                             configurableMapItems.add(item)
                         }
                     }
-                } else {
-                    for (item in mapItems){
-                        item.canShowMapItem(trip)?.let {
-                            if (it) configurableMapItems.add(item)
-                        }
-                    }
-                }
+                } 
                 if (configurableMapItems.isNotEmpty()) {
                     displayMapItem.value = configurableMapItems[0]
                 }
