109f95482c0013f69195a5b194353686456bf860
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2021-02-03 13:59:18 +0100

    Rework distraction fragment.

---
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/commons/views/DistractionItem.kt
@@ -0,0 +1,47 @@
+package com.drivequant.drivekit.ui.commons.views
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.drivequant.drivekit.common.ui.DriveKitUI
+import com.drivequant.drivekit.common.ui.extension.headLine1
+import com.drivequant.drivekit.common.ui.extension.headLine2
+import com.drivequant.drivekit.common.ui.extension.normalText
+import com.drivequant.drivekit.common.ui.extension.smallText
+import com.drivequant.drivekit.ui.R
+import kotlinx.android.synthetic.main.dk_distraction_item.view.*
+
+class DistractionItem : LinearLayout {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    private fun init() {
+        val view = View.inflate(context, R.layout.dk_distraction_item, null)
+        view.text_view_distraction_event.headLine2()
+        view.text_view_distraction_content.smallText(DriveKitUI.colors.complementaryFontColor())
+        view.line_separator.setBackgroundColor(DriveKitUI.colors.neutralColor())
+        addView(
+            view, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
+    }
+
+    fun setDistractionEvent(value: String) {
+        text_view_distraction_event.text = value
+    }
+
+    fun setDistractionContent(value: String) {
+        text_view_distraction_content.text = value
+    }
+}
--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/fragments/DriverDistractionFragment.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/fragments/DriverDistractionFragment.kt
@@ -2,6 +2,7 @@ package com.drivequant.drivekit.ui.tripdetail.fragments
 
 import android.graphics.Color
 import android.graphics.Typeface
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -9,15 +10,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.drivequant.drivekit.common.ui.DriveKitUI
 import com.drivequant.drivekit.common.ui.component.GaugeType
-import com.drivequant.drivekit.common.ui.extension.headLine1
-import com.drivequant.drivekit.common.ui.extension.highlightSmall
+import com.drivequant.drivekit.common.ui.extension.headLine2
 import com.drivequant.drivekit.common.ui.extension.normalText
-import com.drivequant.drivekit.common.ui.utils.FontUtils
-import com.drivequant.drivekit.databaseutils.entity.DriverDistraction
 import com.drivequant.drivekit.ui.R
 import com.drivequant.drivekit.ui.tripdetail.viewmodel.DKTripDetailViewModel
-import com.drivequant.drivekit.ui.tripdetail.viewmodel.MapTraceType
-import com.drivequant.drivekit.ui.tripdetail.viewmodel.DriverDistractionViewModel
 import com.drivequant.drivekit.ui.tripdetail.viewmodel.TripDetailViewModel
 import kotlinx.android.synthetic.main.driver_distraction_fragment.*
 
@@ -38,7 +34,6 @@ class DriverDistractionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.driver_distraction_fragment, container, false)
-        FontUtils.overrideFonts(context, view)
         view.setBackgroundColor(Color.WHITE)
         return view
     }
@@ -53,23 +48,27 @@ class DriverDistractionFragment : Fragment() {
         (savedInstanceState?.getSerializable("viewModel") as TripDetailViewModel?)?.let {
             viewModel = it
         }
+
+        score_gauge.configure(viewModel.getScore()!!, GaugeType.DISTRACTION, Typeface.BOLD)
         gauge_type_title.text = requireContext().getString(R.string.dk_common_distraction)
-        unlockNumberDescription.text = requireContext().getString(R.string.dk_driverdata_unlock_number)
-        unlockDistanceDescription.text = requireContext().getString(R.string.dk_driverdata_unlock_distance)
-        unlockDurationDescription.text = requireContext().getString(R.string.dk_driverdata_unlock_duration)
-
         gauge_type_title.normalText()
-        unlockNumberDescription.normalText()
-        unlockDistanceDescription.normalText()
-        unlockDurationDescription.normalText()
 
-        score_gauge.configure(viewModel.getScore(), GaugeType.DISTRACTION, Typeface.BOLD)
-        unlockNumberEvent.text = viewModel.getUnlockNumberEvent()
-        distanceUnlocked.text = viewModel.getUnlockDistance(requireContext())
-        durationUnlocked.text = viewModel.getUnlockDuration(requireContext())
 
-        unlockNumberEvent.headLine1(DriveKitUI.colors.primaryColor())
-        distanceUnlocked.headLine1(DriveKitUI.colors.primaryColor())
-        durationUnlocked.headLine1(DriveKitUI.colors.primaryColor())
+        distraction_phone_call.setDistractionEvent("1 appel téléphonique")
+        distraction_unlock.setDistractionContent("écran allumé 1m30 et 1,5 km")
+
+        distraction_unlock.setDistractionEvent("Déverouillages de l'écran")
+        distraction_phone_call.setDistractionContent("256 m parcourus")
+
+        phone_call_duration.headLine2(DriveKitUI.colors.primaryColor())
+        unlock_number_event.headLine2(DriveKitUI.colors.primaryColor())
+
+        unlock_number_event.text = "3"
+        phone_call_duration.text = "15 s"
+
+        val phoneCallDurationBackground =  phone_call_duration.background as GradientDrawable
+        val unlockNumberEventBackground =  unlock_number_event.background as GradientDrawable
+        phoneCallDurationBackground.setStroke(5, DriveKitUI.colors.complementaryFontColor())
+        unlockNumberEventBackground.setStroke(5, DriveKitUI.colors.complementaryFontColor())
     }
 }
