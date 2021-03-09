8839169675c99e4b6fe3ab1f5bfcfc1c50f75895
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2021-03-09 14:16:15 +0100

    Update DriveKit modules and fix the way how to check if trip is scored or not.

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/viewmodel/TripData.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/viewmodel/TripData.kt
@@ -1,45 +1,37 @@
 package com.drivequant.drivekit.ui.trips.viewmodel
 
 import com.drivequant.drivekit.common.ui.component.GaugeType
-import com.drivequant.drivekit.common.ui.extension.lessOrEqualsThan
 import com.drivequant.drivekit.databaseutils.entity.Trip
 
 enum class TripData {
     ECO_DRIVING, SAFETY, DISTRACTION, SPEEDING, DURATION, DISTANCE;
 
-    fun isScored(trip: Trip) : Boolean {
-        return when (this){
-            SAFETY, ECO_DRIVING, DISTRACTION, SPEEDING -> rawValue(trip).lessOrEqualsThan(10.toDouble())
-            DISTANCE, DURATION -> true
-        }
+    fun isScored(trip: Trip): Boolean = when (this) {
+        SAFETY, ECO_DRIVING -> !trip.unscored
+        DISTRACTION -> !trip.unscored && trip.driverDistraction != null
+        SPEEDING -> !trip.unscored && trip.speedingStatistics != null
+        DISTANCE, DURATION -> true
     }
 
-    fun displayType() : DisplayType {
-        return when (this) {
-            ECO_DRIVING, SAFETY, DISTRACTION, SPEEDING -> DisplayType.GAUGE
-            DURATION, DISTANCE -> DisplayType.TEXT
-        }
+    fun displayType(): DisplayType = when (this) {
+        ECO_DRIVING, SAFETY, DISTRACTION, SPEEDING -> DisplayType.GAUGE
+        DURATION, DISTANCE -> DisplayType.TEXT
     }
 
-    fun getGaugeType() : GaugeType {
-        return when(this){
-            ECO_DRIVING -> GaugeType.ECO_DRIVING
-            SAFETY -> GaugeType.SAFETY
-            DISTRACTION -> GaugeType.DISTRACTION
-            SPEEDING -> GaugeType.SPEEDING
-            else -> GaugeType.SAFETY
-        }
+    fun getGaugeType(): GaugeType = when (this) {
+        ECO_DRIVING -> GaugeType.ECO_DRIVING
+        SAFETY -> GaugeType.SAFETY
+        DISTRACTION -> GaugeType.DISTRACTION
+        SPEEDING -> GaugeType.SPEEDING
+        else -> GaugeType.SAFETY
     }
 
-    fun rawValue(trip: Trip) : Double? {
-        return when (this){
-            SAFETY -> trip.safety?.safetyScore
-            ECO_DRIVING -> trip.ecoDriving?.score
-            DISTRACTION -> trip.driverDistraction?.score
-            SPEEDING -> trip.speedingStatistics?.score
-            DISTANCE -> trip.tripStatistics?.distance
-            DURATION -> trip.tripStatistics?.duration
-        }
+    fun rawValue(trip: Trip): Double? = when (this) {
+        SAFETY -> trip.safety?.safetyScore
+        ECO_DRIVING -> trip.ecoDriving?.score
+        DISTRACTION -> trip.driverDistraction?.score
+        SPEEDING -> trip.speedingStatistics?.score
+        DISTANCE -> trip.tripStatistics?.distance
+        DURATION -> trip.tripStatistics?.duration
     }
 }
-
