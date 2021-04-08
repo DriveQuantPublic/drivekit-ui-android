6afba6f9f94654ebef7f4d3fcf86094f542639f7
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2021-04-08 20:05:17 +0200

    WIP Add TabLayout for SynthesisCard Viewpager

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/synthesiscards/fragment/DKSynthesisCardViewPagerFragment.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/synthesiscards/fragment/DKSynthesisCardViewPagerFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
+import com.drivequant.drivekit.common.ui.DriveKitUI
+import com.drivequant.drivekit.common.ui.graphical.DKColors
 import com.drivequant.drivekit.ui.R
 import com.drivequant.drivekit.ui.synthesiscards.adapter.DKSynthesisCardFragmentPagerAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.dk_fragment_synthesis_card_viewpager.*
 
 class DKSynthesisCardViewPagerFragment(
@@ -27,5 +32,11 @@ class DKSynthesisCardViewPagerFragment(
             childFragmentManager,
             cards
         )
+
+        if (cards.size > 1) {
+            val tabLayout: TabLayout = view.findViewById(R.id.tabDotsScore)
+            tabLayout.setupWithViewPager(view_pager, true)
+            tabLayout.background = 
+        }
     }
 }
