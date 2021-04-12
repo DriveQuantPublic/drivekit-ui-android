33fc95e99d7ee268fb67298163e86cd91a44cc05
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2021-04-12 19:52:04 +0200

    Design viewpager dots same as whitelabel project

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/synthesiscards/fragment/DKSynthesisCardViewPagerFragment.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/synthesiscards/fragment/DKSynthesisCardViewPagerFragment.kt
@@ -1,14 +1,13 @@
 package com.drivequant.drivekit.ui.synthesiscards.fragment
 
-import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.drivequant.drivekit.common.ui.DriveKitUI
-import com.drivequant.drivekit.common.ui.extension.resSpans
-import com.drivequant.drivekit.common.ui.utils.DKSpannable
+import com.drivequant.drivekit.common.ui.extension.tintDrawable
 import com.drivequant.drivekit.ui.R
 import com.drivequant.drivekit.ui.synthesiscards.adapter.DKSynthesisCardFragmentPagerAdapter
 import com.google.android.material.tabs.TabLayout
@@ -19,9 +18,10 @@ import kotlinx.android.synthetic.main.dk_fragment_synthesis_card_viewpager.*
 class DKSynthesisCardViewPagerFragment : Fragment() {
 
     private var cards = listOf<DKSynthesisCardFragment>()
+    private lateinit var tabLayout: TabLayout
 
     companion object {
-        fun newInstance(cards: List<DKSynthesisCardFragment>) : DKSynthesisCardViewPagerFragment{
+        fun newInstance(cards: List<DKSynthesisCardFragment>): DKSynthesisCardViewPagerFragment {
             val fragment = DKSynthesisCardViewPagerFragment()
             fragment.cards = cards
             return fragment
@@ -44,49 +44,37 @@ class DKSynthesisCardViewPagerFragment : Fragment() {
         )
 
         if (cards.size > 1) {
-            val tabLayout: TabLayout = view.findViewById(R.id.tabDotsScore)
+            tabLayout = view.findViewById(R.id.tabDotsScore)
             tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab) {
-                    val test = tabLayout.getTabAt(tab.position)
-                    if (test != null) {
-                        test.text = DKSpannable().append("·", requireContext().resSpans {
-                            color(DriveKitUI.colors.secondaryColor())
-                            size(R.dimen.dk_text_xxxbig)
-                            typeface(Typeface.BOLD)
-                        }).toSpannable()
-                    }
+                    updateTabLayout(tab.position)
                 }
 
-                override fun onTabUnselected(tab: TabLayout.Tab) {
-                    val test = tabLayout.getTabAt(tab.position)
-                    if (test != null) {
-                        test.text = DKSpannable().append("·", requireContext().resSpans {
-                            color(DriveKitUI.colors.mainFontColor())
-                            size(R.dimen.dk_text_xxbig)
-                            typeface(Typeface.NORMAL)
-                        }).toSpannable()
-                    }
-                }
+                override fun onTabUnselected(tab: TabLayout.Tab) {}
 
                 override fun onTabReselected(tab: TabLayout.Tab) {}
             })
-            for (i in 0 until cards.size){
-                val test = tabLayout.getTabAt(i)
-                test?.let {
-                    test.text = DKSpannable().append("·", requireContext().resSpans {
-                        color(DriveKitUI.colors.mainFontColor())
-                        size(R.dimen.dk_text_xxxbig)
-                        typeface(Typeface.NORMAL)
 
-                    }).toSpannable()
+            tabLayout.setupWithViewPager(view_pager)
+            updateTabLayout(0)
+        }
+    }
+
+    private fun updateTabLayout(position: Int) {
+        for (i in 0 until tabLayout.tabCount) {
+            tabLayout.getTabAt(i)?.let {
+                if (i == position) {
+                    val drawable = ContextCompat.getDrawable(requireContext(),
+                        R.drawable.dk_tab_indicator_selected)?.mutate()
+                    drawable?.tintDrawable(DriveKitUI.colors.secondaryColor())
+                    tabLayout.getTabAt(i)?.icon = drawable
+                } else {
+                    val drawable = ContextCompat.getDrawable(requireContext(),
+                        R.drawable.dk_tab_indicator_default)?.mutate()
+                    drawable?.tintDrawable(DriveKitUI.colors.complementaryFontColor())
+                    tabLayout.getTabAt(i)?.icon = drawable
                 }
             }
-            tabLayout.getTabAt(0)?.customView?.let {
-                it.isSelected = true
-            }
-            tabLayout.setupWithViewPager(view_pager, true)
-            tabLayout.getTabAt(0)?.select()
-
         }
     }
 }
