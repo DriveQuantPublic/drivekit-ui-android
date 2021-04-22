f394d075f26864d0b77114778b5e685adfd866d6
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2021-04-22 14:59:22 +0200

    WIP : Move localizable and add new method to get jump index

--- CommonUI/src/main/java/com/drivequant/drivekit/common/ui/component/ranking/DKDriverRanking.kt
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/component/ranking/DKDriverRanking.kt
@@ -3,15 +3,17 @@ package com.drivequant.drivekit.common.ui.component.ranking
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.text.Spannable
+import com.drivequant.drivekit.common.ui.component.ranking.viewmodel.DriverProgression
 
 interface DKDriverRanking {
     fun getHeaderDisplayType(): RankingHeaderDisplayType
     fun getTitle(): String
-    fun getIcon(): Drawable
-    fun getProgression(previousRank: Int?): Drawable?
+    fun getIcon(context: Context): Drawable?
+    fun getProgression(previousRank: Int?): DriverProgression?
     fun getDriverGlobalRank(context: Context): Spannable
     fun getScoreTitle(): String
     fun getDriverRankingList(): List<DKDriverRankingItem>
+    fun getJumpIndex(): Int
 }
 
 interface DKDriverRankingItem {
