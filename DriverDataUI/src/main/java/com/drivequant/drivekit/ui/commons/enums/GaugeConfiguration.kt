90fa7f45827d3ec016a0b571a629f1d011fa722d
Steven THOMAS-BRONDEAU <steven.thomas-brondeau@drivequant.com>
2021-04-29 15:22:50 +0200

    Fix gauge title in order to round up values For example, if score is 9.65, the title must display 9.7 by rounding half up the value instead of 9.6

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/commons/enums/GaugeConfiguration.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/commons/enums/GaugeConfiguration.kt
@@ -10,6 +10,8 @@ import com.drivequant.drivekit.common.ui.component.DKGaugeType
 import com.drivequant.drivekit.common.ui.extension.removeZeroDecimal
 import com.drivequant.drivekit.common.ui.extension.resSpans
 import com.drivequant.drivekit.common.ui.utils.DKSpannable
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 sealed class GaugeConfiguration(open val value: Double) : DKGaugeConfiguration {
     data class SAFETY(override val value: Double) : GaugeConfiguration(value)
@@ -21,7 +23,7 @@ sealed class GaugeConfiguration(open val value: Double) : DKGaugeConfiguration {
         val text = if (value == 11.0) {
             "-"
         } else {
-            value.removeZeroDecimal()
+            BigDecimal(value).setScale(1, RoundingMode.HALF_UP).toDouble().removeZeroDecimal()
         }
         return DKSpannable().append(text, context.resSpans {
             size(R.dimen.dk_ic_medium)
