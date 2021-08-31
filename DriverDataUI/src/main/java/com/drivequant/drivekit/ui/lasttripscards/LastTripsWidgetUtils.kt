3d3fd52267c6d2dbff20d3f878e9aa3f19f7de59
Mohamed Amine Garci <mohamed.garci@drivequant.com>
2021-08-31 17:52:04 +0200

    Use transportation mode as a parameter in getLastTrips method and fix card height when there is no trips

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/lasttripscards/LastTripsWidgetUtils.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/lasttripscards/LastTripsWidgetUtils.kt
@@ -1,14 +1,22 @@
 package com.drivequant.drivekit.ui.lasttripscards
 
 import com.drivequant.drivekit.databaseutils.Query
+import com.drivequant.drivekit.databaseutils.entity.TransportationMode
 import com.drivequant.drivekit.databaseutils.entity.Trip
 import com.drivequant.drivekit.databaseutils.entity.toTrips
 import com.drivequant.drivekit.driverdata.DriveKitDriverData
 
 internal object LastTripsWidgetUtils {
 
-    fun getLastTrips(limit: Int): List<Trip> = DriveKitDriverData.tripsQuery()
-        .whereIn("transportationMode", listOf(0, 1, 2, 3))
+    @JvmOverloads
+    fun getLastTrips(limit: Int, transportationModes: List<TransportationMode> = listOf(
+            TransportationMode.UNKNOWN,
+            TransportationMode.CAR,
+            TransportationMode.MOTO,
+            TransportationMode.TRUCK
+        )): List<Trip> =
+        DriveKitDriverData.tripsQuery()
+        .whereIn("transportationMode", transportationModes.map { it.value })
         .orderBy("endDate", Query.Direction.DESCENDING).query().limit(limit).executeTrips()
         .toTrips()
 }
