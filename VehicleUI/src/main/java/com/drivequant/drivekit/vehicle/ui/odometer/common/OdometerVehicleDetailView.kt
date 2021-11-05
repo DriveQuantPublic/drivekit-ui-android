c9e70b9137ec88c7aa594cbcebd2a39241d714a3
mohamed-garci <60616229+mohamed-garci@users.noreply.github.com>
2021-11-05 17:12:33 +0100

    Update odometer card style

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/odometer/common/OdometerVehicleDetailView.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/odometer/common/OdometerVehicleDetailView.kt
@@ -1,11 +1,16 @@
 package com.drivequant.drivekit.vehicle.ui.odometer.common
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.drivequant.drivekit.common.ui.DriveKitUI
+import com.drivequant.drivekit.common.ui.extension.highlightBig
+import com.drivequant.drivekit.common.ui.extension.normalText
+import com.drivequant.drivekit.common.ui.extension.smallText
 import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.vehicle.ui.R
 import com.drivequant.drivekit.vehicle.ui.odometer.viewmodel.OdometerItemType
@@ -16,8 +21,7 @@ internal class OdometerVehicleDetailView : LinearLayout {
 
     fun init(attrs: AttributeSet?) {
         val view = View.inflate(context, R.layout.dk_layout_odometer_distance_item, null)
-        addView(
-            view, ViewGroup.LayoutParams(
+        addView(view, ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
@@ -27,7 +31,10 @@ internal class OdometerVehicleDetailView : LinearLayout {
                 context.obtainStyledAttributes(attrs, R.styleable.OdometerVehicleDetailView, 0, 0)
             try {
                 a.getString(R.styleable.OdometerVehicleDetailView_odometerTitle)?.let {
-                    text_view_odometer_distance_title.text = DKResource.convertToString(context, it)
+                    text_view_odometer_distance_title.apply {
+                        text = DKResource.convertToString(context, it)
+                        setTextColor(Color.BLACK)
+                    }
                 }
                 a.getDrawable(R.styleable.OdometerVehicleDetailView_odometerCornerIcon)?.let {
                     image_view_info.setImageDrawable(it)
@@ -35,7 +42,7 @@ internal class OdometerVehicleDetailView : LinearLayout {
             } finally {
                 a.recycle()
             }
-            setStyle()
+            dk_view_separator.setBackgroundColor(DriveKitUI.colors.neutralColor())
         }
     }
 
@@ -47,16 +54,15 @@ internal class OdometerVehicleDetailView : LinearLayout {
         init(attrs)
     }
 
-    private fun setStyle() {
-        image_view_info.apply {
-            setColorFilter(DriveKitUI.colors.secondaryColor())
-        }
-        dk_view_separator.setBackgroundColor(DriveKitUI.colors.neutralColor())
-    }
-
     fun configureOdometerItem(viewModel: OdometerItemViewModel, odometerItemType: OdometerItemType, listener: OdometerDrawableListener) {
-        text_view_odometer_distance_description.text = viewModel.getDescription(context, odometerItemType)
-        text_view_odometer_distance_value.text = viewModel.getDistance(context, odometerItemType)
+        text_view_odometer_distance_description.apply {
+            text = viewModel.getDescription(context, odometerItemType)
+            smallText(ContextCompat.getColor(context, R.color.dkGrayColor))
+        }
+        text_view_odometer_distance_value.apply {
+            text = viewModel.getDistance(context, odometerItemType)
+            highlightBig(ContextCompat.getColor(context, R.color.dkGrayColor))
+        }
 
         image_view_info.apply {
             setColorFilter(DriveKitUI.colors.secondaryColor())
