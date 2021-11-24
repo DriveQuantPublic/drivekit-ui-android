a8dcc9db191e85a1e668df5eedde143e5c10e9c3
mohamed-garci <60616229+mohamed-garci@users.noreply.github.com>
2021-11-24 17:08:19 +0100

    Vehicle UI : Optimize vehicles synchronization (#142)
    * Get vehicles from cache and then start a new sync
    
    * Refresh list when adding a new vehicle.
    
    * Integrate DriveKitVehicleListener in VehicleUI module.
    
    * Implement DriveKitVehicleListener in Vehicle synchronization activity

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/activity/VehiclesListActivity.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/activity/VehiclesListActivity.kt
@@ -1,14 +1,29 @@
 package com.drivequant.drivekit.vehicle.ui.vehicles.activity
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.drivequant.drivekit.core.SynchronizationType
+import com.drivequant.drivekit.vehicle.DriveKitVehicleListener
 import com.drivequant.drivekit.vehicle.ui.R
 import com.drivequant.drivekit.vehicle.ui.vehicles.fragment.VehiclesListFragment
 
-class VehiclesListActivity : AppCompatActivity() {
+class VehiclesListActivity : AppCompatActivity(), DriveKitVehicleListener {
+
+   private lateinit var fragment: VehiclesListFragment
+
+    companion object {
+        fun launchActivity(context: Context) {
+            val intent = Intent(context, VehiclesListActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            context.startActivity(intent)
+        }
+    }
+
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +36,24 @@ class VehiclesListActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        fragment = VehiclesListFragment()
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, VehiclesListFragment())
+            .replace(R.id.container, fragment)
             .commit()
     }
 
-    fun updateTitle(title: String){
+    fun updateTitle(title: String) {
         this.title = title
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun vehiclesUpdated() {
+        if (this::fragment.isInitialized) {
+            fragment.updateVehicles(SynchronizationType.CACHE)
+        }
     }
 }
