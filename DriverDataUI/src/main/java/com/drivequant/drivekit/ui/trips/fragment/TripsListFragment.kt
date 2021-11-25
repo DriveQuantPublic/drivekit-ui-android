5789e54cca69d7134afcb609cee61a379a7c109f
mohamed-garci <60616229+mohamed-garci@users.noreply.github.com>
2021-11-25 15:30:56 +0100

    Clean code.

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/fragment/TripsListFragment.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/trips/fragment/TripsListFragment.kt
@@ -1,12 +1,10 @@
 package com.drivequant.drivekit.ui.trips.fragment
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
+
 import androidx.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.view.*
