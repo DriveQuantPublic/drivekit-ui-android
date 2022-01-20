f0e34ae6a978bcc91c15a6ec1a137d394d7a12a0
Mohamed Amine Garci <aminegarci100@gmail.com>
2022-01-20 11:09:45 +0100

    Clean test snippet.

--- app/src/main/java/com/drivekit/demoapp/activity/SynthesisCardExampleActivity.kt
+++ app/src/main/java/com/drivekit/demoapp/activity/SynthesisCardExampleActivity.kt
@@ -3,11 +3,9 @@ package com.drivekit.demoapp.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.drivekit.drivekitdemoapp.R
-import com.drivequant.drivekit.ui.DriverDataSynthesisCardsUI
 import com.drivequant.drivekit.ui.DriverDataUI
 import com.drivequant.drivekit.ui.SynthesisCardsViewListener
-import com.drivequant.drivekit.ui.synthesiscards.LastTripsSynthesisCard
-import com.drivequant.drivekit.ui.synthesiscards.SynthesisCard
+import com.drivequant.drivekit.ui.synthesiscards.*
 import com.drivequant.drivekit.ui.synthesiscards.fragment.DKSynthesisCardViewPagerFragment
 
 class SynthesisCardExampleActivity : AppCompatActivity() {
@@ -15,8 +13,8 @@ class SynthesisCardExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.driver_data_synthesis_cards_activity)
-     /*
-     DriverDataUI.getLastTripsSynthesisCardsView(
+
+        DriverDataUI.getLastTripsSynthesisCardsView(
             listOf(
                 LastTripsSynthesisCard.SAFETY,
                 LastTripsSynthesisCard.ECO_DRIVING,
@@ -30,20 +28,6 @@ class SynthesisCardExampleActivity : AppCompatActivity() {
                         .commit()
                 }
             })
-         */
-
-        //TODO Remove code snippet once feature is done
-        DriverDataSynthesisCardsUI.getSynthesisCardsView(listOf(
-            CustomSynthesisCard()),
-            object : SynthesisCardsViewListener {
-                override fun onViewLoaded(fragment: DKSynthesisCardViewPagerFragment) {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.container, fragment)
-                        .commit()
-                }
-            })
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
     }
