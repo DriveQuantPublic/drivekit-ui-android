046835fd51a6b2ebf422ae3d8be0f75d580cb9da
Mohamed Amine Garci <aminegarci100@gmail.com>
2022-01-25 12:06:12 +0100

    Clean and improve existant code.

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicledetail/adapter/VehicleFieldsListAdapter.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicledetail/adapter/VehicleFieldsListAdapter.kt
@@ -10,10 +10,8 @@ import com.drivequant.drivekit.vehicle.ui.vehicledetail.viewmodel.GroupField
 import com.drivequant.drivekit.vehicle.ui.vehicledetail.viewmodel.VehicleDetailViewModel
 
 class VehicleFieldsListAdapter(
-    var context: Context,
-    var viewModel: VehicleDetailViewModel,
-    private var groupFields: List<GroupField>)
-    : RecyclerView.Adapter<VehicleFieldViewHolder>() {
+    val context: Context,
+    val viewModel: VehicleDetailViewModel) : RecyclerView.Adapter<VehicleFieldViewHolder>() {
 
     override fun onCreateViewHolder(viewgroup: ViewGroup, position: Int): VehicleFieldViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.view_vehicle_field_item_list, viewgroup, false)
@@ -21,14 +19,12 @@ class VehicleFieldsListAdapter(
     }
 
     override fun onBindViewHolder(holder: VehicleFieldViewHolder, position: Int) {
-        holder.bind(groupFields[position])
+        holder.bind(viewModel.groupFields[position])
     }
 
-    override fun getItemCount(): Int {
-        return groupFields.size
-    }
+    override fun getItemCount() = viewModel.groupFields.size
 
-    fun setGroupFields(groupFields: List<GroupField>) {
-        this.groupFields = groupFields
+    fun setGroupFields(groupFields: MutableList<GroupField>) {
+        this.viewModel.groupFields = groupFields
     }
 }
