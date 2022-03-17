c300a2404a4dbfacd03accfd8123af094e37f73d
Steven <56969493+Steven-DriveQuant@users.noreply.github.com>
2022-03-17 16:48:05 +0100

    Feature/crash feedback UI (#177)
    * Update strings
    
    * Create generic button for crash feedback action
    
    * Continue Step1 activity layout, introduce PieChart
    
    * Create custom background for PieChart
    
    * Add rounded slice renderer for PieChart
    
    * Update feedback button design
    
    * Finish first screen with timer
    
    * Add tag, remove toolbar
    
    * Finish UI
    
    * WIP Crash Feedback integration
    
    * Clean UI and prepare to ask for review
    
    * Clean code
    
    * Fix wrong key
    
    * Restore DKCrashAlert to SILENCE
    
    * Fix countdown issue
    
    * Set timer to 1 minute in every case
    
    * Only display seconds in Timer
    
    * Let phone screen awake if crash feedback UI is shown
    
    * Avoid user to press back during crash feedback

---
+++ TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/crashfeedback/activity/CrashFeedbackStep1Activity.kt
@@ -0,0 +1,161 @@
+package com.drivequant.drivekit.tripanalysis.crashfeedback.activity
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProviders
+import com.drivekit.tripanalysis.ui.R
+import com.drivequant.drivekit.common.ui.DriveKitUI
+import com.drivequant.drivekit.common.ui.extension.*
+import com.drivequant.drivekit.common.ui.utils.*
+import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysis
+import com.drivequant.drivekit.tripanalysis.crashfeedback.view.RoundedSlicesPieChartRenderer
+import com.drivequant.drivekit.tripanalysis.crashfeedback.viewmodel.CrashFeedbackStep1ViewModel
+import com.drivequant.drivekit.tripanalysis.model.crashdetection.CrashFeedbackStatus
+import com.drivequant.drivekit.tripanalysis.model.crashdetection.CrashUserFeedbackListener
+import com.drivequant.drivekit.tripanalysis.service.crashdetection.feedback.CrashFeedbackSeverity
+import com.drivequant.drivekit.tripanalysis.service.crashdetection.feedback.CrashFeedbackType
+import com.drivequant.drivekit.tripanalysis.service.crashdetection.feedback.DKCrashFeedbackListener
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.highlight.Highlight
+import kotlinx.android.synthetic.main.dk_layout_activity_crash_feedback_step1.*
+import java.util.*
+
+
+class CrashFeedbackStep1Activity : BaseCrashFeedbackActivity() {
+
+    private lateinit var viewModel: CrashFeedbackStep1ViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        DriveKitUI.analyticsListener?.trackScreen(
+            DKResource.convertToString(
+                this,
+                "dk_tag_trip_analysis_crash_feedback_step1"
+            ), javaClass.simpleName
+        )
+
+        DriveKitTripAnalysis.setCrashFeedbackTimer(60) // 1 minute
+
+        setContentView(R.layout.dk_layout_activity_crash_feedback_step1)
+
+        if (!this::viewModel.isInitialized) {
+            viewModel = ViewModelProviders.of(this).get(CrashFeedbackStep1ViewModel::class.java)
+        }
+
+        DriveKitTripAnalysis.setCrashFeedbackListener(object : DKCrashFeedbackListener {
+            override fun onProgress(currentSecond: Int, timeoutSecond: Int) {
+                runOnUiThread {
+                    updateTimer(currentSecond, timeoutSecond)
+                }
+            }
+
+            override fun timeoutReached() {
+                runOnUiThread {
+                    finish()
+                }
+            }
+        })
+
+        initTitle()
+        initDescription()
+        initTimer()
+    }
+
+    private fun initTitle() {
+        viewModel.getTitleResId().let {
+            text_view_title.text = this.getText(it)
+            text_view_title.highlightMedium()
+        }
+    }
+
+    private fun initDescription() {
+        viewModel.getDescriptionResId().let {
+            text_view_description.text = this.getText(it)
+            text_view_description.normalText(DriveKitUI.colors.complementaryFontColor())
+        }
+    }
+
+    private fun initTimer() {
+        timer.apply {
+            setTouchEnabled(false)
+            isRotationEnabled = false
+            description = null
+            legend.isEnabled = false
+            holeRadius = 90f
+            isDrawHoleEnabled = true
+            transparentCircleRadius = 61f
+            renderer = RoundedSlicesPieChartRenderer(
+                this,
+                this.animator,
+                this.viewPortHandler
+            )
+            setNoDataText("")
+            invalidate()
+        }
+    }
+
+    private fun updateTimer(duration: Int, total: Int) {
+        val data = DKDataFormatter.formatDuration(this, (total - duration).toDouble(), maxUnit = DurationUnit.SECOND)
+        val spannable = DKSpannable()
+        data.forEach {
+            when (it) {
+                is FormatType.VALUE -> spannable.append(
+                    it.value,
+                    this.resSpans {
+                        color(DriveKitUI.colors.fontColorOnPrimaryColor())
+                        typeface(Typeface.BOLD)
+                        size(R.dimen.dk_text_xbig)
+                    })
+                else -> {
+                    // do nothing
+                }
+            }
+        }
+
+        timer.apply {
+            highlightValues(arrayOf(Highlight(0f, 0f, 0)))
+            centerText = spannable.toSpannable().toString()
+            val textSize = context.resources.getDimension(com.drivequant.drivekit.common.ui.R.dimen.dk_text_small)
+            setCenterTextColor(DriveKitUI.colors.mainFontColor())
+            setCenterTextTypeface(DriveKitUI.primaryFont(context))
+            setCenterTextSize(textSize)
+        }
+        val entries: MutableList<PieEntry> = ArrayList()
+        entries.add(PieEntry((60-duration).toFloat()))
+        entries.add(PieEntry(duration.toFloat()))
+        val pieDataSet = PieDataSet(entries, null)
+        pieDataSet.setColors(intArrayOf(R.color.dkCrashFeedbackAssistance, R.color.dkCrashFeedbackAssistance_10), this)
+        val pieData = PieData(pieDataSet)
+        pieData.setDrawValues(false)
+        pieDataSet.selectionShift = 0f
+        pieDataSet.sliceSpace = 0f
+        timer.data = pieData
+        timer.invalidate()
+    }
+
+    fun onNoCrashButtonClicked(view: View) {
+        DriveKitTripAnalysis.stopCrashFeedbackTimer()
+        startActivity(Intent(this, CrashFeedbackStep2Activity::class.java))
+        finish()
+    }
+
+    fun onAssistanceButtonClicked(view: View) {
+        dismissKeyguard()
+        launchPhoneCall() // Do not wait the webservice response to make the phone call !
+        DriveKitTripAnalysis.stopCrashFeedbackTimer()
+        DriveKitTripAnalysis.sendUserFeedback(
+            feedbackType = CrashFeedbackType.CRASH_CONFIRMED,
+            severity = CrashFeedbackSeverity.CRITICAL,
+            listener = object : CrashUserFeedbackListener {
+                override fun onFeedbackSent(status: CrashFeedbackStatus) {
+                    finish()
+                }
+            }
+        )
+    }
+}
---
+++ TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/crashfeedback/view/CrashFeedbackButton.kt
@@ -0,0 +1,151 @@
+package com.drivequant.drivekit.tripanalysis.crashfeedback.view
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Typeface
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.content.ContextCompat
+import com.drivekit.tripanalysis.ui.R
+import com.drivequant.drivekit.common.ui.extension.headLine1
+
+
+internal class CrashFeedbackButton : FrameLayout {
+
+    private lateinit var container: FrameLayout
+    private lateinit var button: LinearLayout
+    private lateinit var textView: TextView
+
+    private var borderColor: Int? = null
+    private var bgColor: Int? = null
+    private var iconResId: Int? = null
+    private var titleResId: Int? = null
+
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    constructor(
+        context: Context, attrs: AttributeSet?
+    ) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        container = View.inflate(context, R.layout.dk_layout_crash_feedback_button, null) as FrameLayout
+        button = container.findViewById(R.id.button)
+        textView = container.findViewById(R.id.text_view)
+
+        if (attrs != null) {
+            val typedArray = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.DKCrashFeedbackButtonItemView,
+                0,
+                0
+            )
+            for (i in 0 until typedArray.indexCount) {
+                val attr = typedArray.getIndex(i)
+                val resId = typedArray.getResourceId(attr, -1)
+                when (attr) {
+                    R.styleable.DKCrashFeedbackButtonItemView_dkCrashFeedbackButtonItemDrawable -> {
+                        iconResId = resId
+                    }
+                    R.styleable.DKCrashFeedbackButtonItemView_dkCrashFeedbackButtonItemTitle -> {
+                        titleResId = resId
+                    }
+                    R.styleable.DKCrashFeedbackButtonItemView_dkCrashFeedbackButtonBorder -> {
+                        borderColor = resId
+                    }
+                    R.styleable.DKCrashFeedbackButtonItemView_dkCrashFeedbackButtonBackground -> {
+                        bgColor = resId
+                    }
+                }
+            }
+            typedArray.recycle()
+            val b = context.obtainStyledAttributes(
+                attrs,
+                intArrayOf(android.R.attr.inputType),
+                0,
+                0
+            )
+            b.recycle()
+        }
+        tintBorder()
+        setButtonBackground()
+        setText()
+        setItemDrawable()
+        addView(
+            container, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
+    }
+
+    private fun tintBorder() {
+        borderColor?.let {
+            if (borderColor != null) {
+                (container.background as GradientDrawable).setStroke(
+                    6,
+                    ContextCompat.getColor(context, it)
+                )
+            }
+        }
+    }
+
+    private fun setItemDrawable() {
+        iconResId?.let {
+            if (iconResId != 0) {
+                val drawable: Drawable? = ContextCompat.getDrawable(context, it)
+                if (drawable != null) {
+                    val bitmap = (drawable as BitmapDrawable).bitmap
+                    val size = context.resources.getDimension(R.dimen.dk_ic_xbig).toInt()
+                    BitmapDrawable(
+                        context.resources,
+                        Bitmap.createScaledBitmap(bitmap, size, size, true)
+                    ).let { bitmapDrawable ->
+                        textView.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            null,
+                            null,
+                            bitmapDrawable,
+                            null
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setText() {
+        titleResId?.let {
+            if (titleResId != 0) {
+                textView.setText(it)
+                borderColor?.let { borderColor ->
+                    textView.headLine1(ContextCompat.getColor(context, borderColor))
+                    textView.typeface = Typeface.DEFAULT
+                }
+            }
+        }
+    }
+
+    private fun setButtonBackground() {
+        bgColor?.let {
+            if (bgColor != 0) {
+                button.setBackgroundColor(ContextCompat.getColor(context, it))
+            }
+        }
+    }
+}
