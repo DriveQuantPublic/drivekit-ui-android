70943096b3521f0e8e87bb37471c7704157d1b33
Steven <steven@drivequant.com>
2022-04-26 19:04:13 +0200

    Add toolbar on Streaks and Badges activities

--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/activity/BadgeListActivity.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/badges/activity/BadgeListActivity.kt
@@ -2,6 +2,8 @@ package com.drivequant.drivekit.driverachievement.ui.badges.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.driverachievement.ui.R
 import com.drivequant.drivekit.driverachievement.ui.badges.fragment.BadgesListFragment
 
@@ -11,9 +13,24 @@ class BadgeListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dk_activity_badges_list)
 
+        setToolbar()
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, BadgesListFragment())
             .commit()
+    }
+
+    private fun setToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.dk_toolbar)
+        setSupportActionBar(toolbar)
+        title = DKResource.convertToString(this, "menu_mybadges")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/streaks/activity/StreaksListActivity.kt
+++ DriverAchievementUI/src/main/java/com/drivequant/drivekit/driverachievement/ui/streaks/activity/StreaksListActivity.kt
@@ -2,7 +2,9 @@ package com.drivequant.drivekit.driverachievement.ui.streaks.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.drivequant.drivekit.common.ui.navigation.DriveKitNavigationController
+import com.drivequant.drivekit.common.ui.utils.DKResource
 import com.drivequant.drivekit.driverachievement.ui.R
 
 class StreaksListActivity : AppCompatActivity() {
@@ -10,11 +12,27 @@ class StreaksListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dk_activity_streaks_list)
+
+        setToolbar()
+
         DriveKitNavigationController.driverAchievementUIEntryPoint?.let {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.container, it.createStreakListFragment())
                 .commit()
         }
+    }
+
+    private fun setToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.dk_toolbar)
+        setSupportActionBar(toolbar)
+        title = DKResource.convertToString(this, "dk_achievements_menu_streaks")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
