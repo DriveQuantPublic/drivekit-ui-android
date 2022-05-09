2b02afdfc0d4c6baa6f144279a2092c919b5b087
Steven <steven@drivequant.com>
2022-05-09 11:17:41 +0200

    Update app icon

--- app/src/main/java/com/drivekit/demoapp/DriveKitDemoApplication.kt
+++ app/src/main/java/com/drivekit/demoapp/DriveKitDemoApplication.kt
@@ -17,7 +17,7 @@ class DriveKitDemoApplication : Application() {
     companion object {
         fun showNotification(context: Context, message: String) {
             val builder = NotificationCompat.Builder(context, "notif_channel")
-                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setSmallIcon(R.mipmap.ic_launcher) // TODO ic_notification
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(message)
                 .setStyle(NotificationCompat.BigTextStyle().bigText(message))
