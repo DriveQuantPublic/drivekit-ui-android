8a01217a7f1f749e8bd8936ffccf9f5f13e016eb
Steven <steven@drivequant.com>
2022-11-07 18:10:40 +0100

    Update GraphAxisConfig to fit with iOS

--- DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/GraphAxisConfig.kt
+++ DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/GraphAxisConfig.kt
@@ -3,5 +3,5 @@ package com.drivequant.drivekit.timeline.ui.component.graph
 internal object GraphAxisConfig {
     var min: Double = 0.0
     var max: Double = 0.0
-    var labelCount: Int? = 0
+    var labels: List<String> = listOf()
 }
