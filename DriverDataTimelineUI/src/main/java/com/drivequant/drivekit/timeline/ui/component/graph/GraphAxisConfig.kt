89a987a099b604c75357f496c6358b1f25cddc19
Steven <steven@drivequant.com>
2022-12-20 17:05:04 +0100

    Continue GraphViewBase declaration

--- DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/GraphAxisConfig.kt
+++ DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/GraphAxisConfig.kt
@@ -1,7 +1,22 @@
 package com.drivequant.drivekit.timeline.ui.component.graph
 
 internal data class GraphAxisConfig(
-    val min: Double?,
-    val max: Double?,
-    val labels: List<String>?
+    val min: Double,
+    val max: Double,
+    val labels: LabelType,
 )
+
+internal sealed class LabelType {
+    data class RAW_VALUES(val labelCount: Int) : LabelType()
+    data class CUSTOM_LABELS(val labels: List<String>) : LabelType()
+
+    val count = when (this) {
+        is CUSTOM_LABELS -> labels.size
+        is RAW_VALUES -> labelCount
+    }
+
+    val titles = when (this) {
+        is CUSTOM_LABELS -> labels
+        is RAW_VALUES -> null
+    }
+}
--- DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/view/GraphViewBase.kt
+++ DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/component/graph/view/GraphViewBase.kt
@@ -1,9 +1,19 @@
 package com.drivequant.drivekit.timeline.ui.component.graph.view
 
 import android.content.Context
+import android.graphics.Canvas
 import android.widget.LinearLayout
+import com.drivequant.drivekit.common.ui.extension.format
+import com.drivequant.drivekit.timeline.ui.component.graph.GraphAxisConfig
 import com.drivequant.drivekit.timeline.ui.component.graph.viewmodel.GraphViewModel
 import com.github.mikephil.charting.charts.BarLineChartBase
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.renderer.XAxisRenderer
+import com.github.mikephil.charting.utils.MPPointF
+import com.github.mikephil.charting.utils.Transformer
+import com.github.mikephil.charting.utils.ViewPortHandler
 
 internal abstract class GraphViewBase(context: Context): LinearLayout(context) {
 
@@ -23,4 +33,53 @@ internal abstract class GraphViewBase(context: Context): LinearLayout(context) {
     abstract fun getChartView(): BarLineChartBase<*>
 
     abstract fun setupData()
+}
+
+internal class GraphAxisFormatter: ValueFormatter() {
+    private lateinit var config: GraphAxisConfig
+
+    fun init(config: GraphAxisConfig) {
+        this.config = config
+    }
+
+    fun stringForValue(value: Double, axis: AxisBase?): String {
+        this.config.labels.titles?.let { labels ->
+            val index = (value - this.config.min).toInt()
+            if (index >= 0 && index < labels.size) {
+                return labels[index]
+            }
+        }
+        return value.format(1)
+    }
+}
+
+internal class DKAxisRenderer(
+    val config: GraphAxisConfig,
+    viewPortHandler: ViewPortHandler,
+    xAxis: XAxis,
+    transformer: Transformer,
+) : XAxisRenderer(viewPortHandler, xAxis, transformer) {
+
+    var selectedIndex: Int? = null
+
+    init {
+        // TODO super.init() not needed ?
+    }
+
+    override fun drawLabel(
+        c: Canvas?,
+        formattedLabel: String?,
+        x: Float,
+        y: Float,
+        anchor: MPPointF?,
+        angleDegrees: Float
+    ) {
+        super.drawLabel(c, formattedLabel, x, y, anchor, angleDegrees)
+
+        val index = this.config.labels.titles?.indexOfFirst { it == formattedLabel }
+        if (index != null && index == selectedIndex) {
+            // TODO draw a background with a pill shape when the label is the selected one
+        }
+        super.drawLabel(c, formattedLabel, x, y, anchor, angleDegrees)
+    }
 }
