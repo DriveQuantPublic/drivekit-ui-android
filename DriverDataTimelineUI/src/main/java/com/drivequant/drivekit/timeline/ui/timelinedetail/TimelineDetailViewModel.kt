1783c2fb5b4801da9573729ab4c94152f273c547
Steven <steven@drivequant.com>
2023-01-03 11:47:53 +0100

    Update TimelineDetail VM as AndroidViewModel

--- DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/timelinedetail/TimelineDetailViewModel.kt
+++ DriverDataTimelineUI/src/main/java/com/drivequant/drivekit/timeline/ui/timelinedetail/TimelineDetailViewModel.kt
@@ -1,7 +1,19 @@
 package com.drivequant.drivekit.timeline.ui.timelinedetail
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-internal class TimelineDetailViewModel: ViewModel() {
+internal class TimelineDetailViewModel(application: Application): AndroidViewModel(application) {
 
+
+
+    @Suppress("UNCHECKED_CAST")
+    class TimelineDetailViewModelFactory(private val application: Application) :
+        ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return TimelineDetailViewModel(application) as T
+        }
+    }
 }
