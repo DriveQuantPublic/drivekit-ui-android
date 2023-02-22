4ebdccacc4b178938824b1bfeaad1aa1ceea846c
Steven <steven@drivequant.com>
2023-02-22 18:22:49 +0100

    Manage and design My Synthesis Card with real data

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/component/scorecard/MySynthesisScoreCardView.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/component/scorecard/MySynthesisScoreCardView.kt
@@ -1,16 +1,22 @@
 package com.drivequant.drivekit.ui.mysynthesis.component.scorecard
 
 import android.content.Context
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.drivequant.drivekit.common.ui.DriveKitUI
 import com.drivequant.drivekit.common.ui.extension.*
+import com.drivequant.drivekit.common.ui.utils.DKSpannable
+import com.drivequant.drivekit.core.common.DKPeriod
+import com.drivequant.drivekit.driverdata.timeline.DKScoreEvolutionTrend
 import com.drivequant.drivekit.ui.R
 import kotlinx.android.synthetic.main.dk_my_synthesis_score_card_view.view.*
 
-internal class MySynthesisScoreCardView : LinearLayout {
+// todo internal ?
+class MySynthesisScoreCardView : LinearLayout {
 
     private lateinit var viewModel: MySynthesisScoreCardViewModel
 
@@ -60,9 +66,61 @@ internal class MySynthesisScoreCardView : LinearLayout {
         }
     }
 
+
     private fun update() {
-        score_card_title.headLine2(DriveKitUI.colors.primaryColor())
-        score_card_subtitle.highlightBig(DriveKitUI.colors.primaryColor())
-        score_card_evolution_text.normalText(DriveKitUI.colors.complementaryFontColor())
+        score_card_title.apply {
+            headLine2(DriveKitUI.colors.primaryColor())
+            score_card_title.text = context.getString(viewModel.getCardTitleResId())
+        }
+
+        this.viewModel.scoreSynthesis?.let { scoreSynthesis ->
+
+            //TODO cut in separate methods
+            val scoreValue = scoreSynthesis.scoreValue
+            val subtitleTextColor = if (scoreValue != null) DriveKitUI.colors.primaryColor() else DriveKitUI.colors.complementaryFontColor()
+
+            score_card_subtitle.text = DKSpannable().computeScoreOnTen(scoreValue).toSpannable()
+            score_card_subtitle.highlightBig(subtitleTextColor)
+
+            val previousScore = scoreSynthesis.previousScoreValue
+
+            score_card_evolution_text.normalText()
+            if (previousScore != null) { // has previous score
+                when (this.viewModel.selectedPeriod) {
+                    DKPeriod.WEEK -> R.string.dk_driverdata_mysynthesis_previous_week
+                    DKPeriod.MONTH -> R.string.dk_driverdata_mysynthesis_previous_month
+                    DKPeriod.YEAR -> R.string.dk_driverdata_mysynthesis_previous_year
+                }.let {
+                    score_card_evolution_text.text = DKSpannable().append(context.getString(it)).space().computeScoreOnTen(previousScore).toSpannable()
+                }
+            } else {
+                // TODO no previous score
+            }
+            score_card_evolution_text.normalText(DriveKitUI.colors.complementaryFontColor())
+
+            var drawableResId: Int = R.drawable.dk_driver_data_trend_steady
+            val iconColor = if (scoreValue != null && previousScore != null) DriveKitUI.colors.primaryColor() else DriveKitUI.colors.complementaryFontColor()
+            scoreSynthesis.evolutionTrend?.let { trend ->
+                when (trend) {
+                    DKScoreEvolutionTrend.UP -> R.drawable.dk_driver_data_trend_positive
+                    DKScoreEvolutionTrend.DOWN -> R.drawable.dk_driver_data_trend_negative
+                    DKScoreEvolutionTrend.SAME -> R.drawable.dk_driver_data_trend_steady
+                }.let {
+                    drawableResId = it
+                }
+            }
+            val drawable = ContextCompat.getDrawable(context, drawableResId)
+            drawable?.let { icon ->
+                icon.tintDrawable(iconColor)
+                score_card_icon.setImageDrawable(icon)
+            }
+        }
+    }
+
+    private fun DKSpannable.computeScoreOnTen(score: Double?): DKSpannable {
+        val scoreText = (score?.format(1) ?: "-")
+            .plus(" ")
+            .plus(context.getString(R.string.dk_common_unit_score))
+        return this.append(scoreText, context.resSpans { typeface(Typeface.BOLD) })
     }
 }
--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/component/scorecard/MySynthesisScoreCardViewModel.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/component/scorecard/MySynthesisScoreCardViewModel.kt
@@ -1,4 +1,45 @@
 package com.drivequant.drivekit.ui.mysynthesis.component.scorecard
 
-internal class MySynthesisScoreCardViewModel {
+import androidx.annotation.StringRes
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.drivequant.drivekit.core.common.DKPeriod
+import com.drivequant.drivekit.core.scoreslevels.DKScoreType
+import com.drivequant.drivekit.driverdata.timeline.DKDriverTimeline
+import com.drivequant.drivekit.driverdata.timeline.DKScoreSynthesis
+import com.drivequant.drivekit.driverdata.timeline.getDriverScoreSynthesis
+import com.drivequant.drivekit.ui.R
+import java.util.*
+
+// TODO internal?
+class MySynthesisScoreCardViewModel : ViewModel() {
+
+    val changeObserver: MutableLiveData<Any> = MutableLiveData()
+
+    lateinit var selectedScoreType: DKScoreType
+    lateinit var selectedPeriod: DKPeriod
+    lateinit var driverTimeline: DKDriverTimeline
+    var scoreSynthesis: DKScoreSynthesis? = null
+    private lateinit var selectedDate: Date
+
+    fun configure(scoreType: DKScoreType?, period: DKPeriod?, driverTimeline: DKDriverTimeline?, selectedDate: Date?) {
+        if (scoreType != null && period != null && driverTimeline != null && selectedDate != null) {
+            this.selectedScoreType = scoreType
+            this.selectedPeriod = period
+            this.driverTimeline = driverTimeline
+            this.selectedDate = selectedDate
+            this.scoreSynthesis = driverTimeline.getDriverScoreSynthesis(this.selectedScoreType, selectedDate)
+        } else {
+            // TODO empty Timeline
+        }
+        this.changeObserver.postValue(Any())
+    }
+
+    @StringRes
+    fun getCardTitleResId() = when (this.selectedScoreType) {
+        DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_score
+        DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_score
+        DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_score
+        DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_score
+    }
 }
