3932c240bda62e84f52870d20ba9b4ab21342e7d
Steven <steven@drivequant.com>
2023-03-03 11:28:09 +0100

    Move method in extension

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/MySynthesisFragment.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/mysynthesis/MySynthesisFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
@@ -24,6 +23,7 @@ import com.drivequant.drivekit.common.ui.utils.DKScoreTypeLevel
 import com.drivequant.drivekit.common.ui.utils.DKSpannable
 import com.drivequant.drivekit.core.scoreslevels.DKScoreType
 import com.drivequant.drivekit.ui.R
+import com.drivequant.drivekit.ui.extension.getScoreLevelDescription
 import com.drivequant.drivekit.ui.mysynthesis.component.scorecard.MySynthesisScoreCardView
 
 internal class MySynthesisFragment : Fragment() {
@@ -229,7 +229,7 @@ internal class MySynthesisFragment : Fragment() {
                 size(R.dimen.dk_text_normal)
             }).space()
                 .append(
-                    getString(getScoreLevelDescription(dkScoreType, scoreLevel)),
+                    getString(scoreLevel.getScoreLevelDescription(dkScoreType)),
                     context.resSpans {
                         color(DriveKitUI.colors.complementaryFontColor())
                         typeface(Typeface.NORMAL)
@@ -246,54 +246,6 @@ internal class MySynthesisFragment : Fragment() {
         )
         view.layoutParams = params
     }
-
-    //TODO viewmodel or extension
-    @StringRes
-    private fun getScoreLevelDescription(dkScoreType: DKScoreType, scoreLevel: DKScoreTypeLevel) =
-        when (scoreLevel) {
-            DKScoreTypeLevel.EXCELLENT -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_excellent
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_excellent
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_excellent
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_excellent
-            }
-            DKScoreTypeLevel.VERY_GOOD -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_very_good
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_very_good
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_very_good
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_very_good
-            }
-            DKScoreTypeLevel.GREAT -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_good
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_good
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_good
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_good
-            }
-            DKScoreTypeLevel.MEDIUM -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_average
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_average
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_average
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_average
-            }
-            DKScoreTypeLevel.NOT_GOOD -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_low
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_low
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_low
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_low
-            }
-            DKScoreTypeLevel.BAD -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_bad
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_bad
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_bad
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_bad
-            }
-            DKScoreTypeLevel.VERY_BAD -> when (dkScoreType) {
-                DKScoreType.SAFETY -> R.string.dk_driverdata_mysynthesis_safety_level_very_bad
-                DKScoreType.ECO_DRIVING -> R.string.dk_driverdata_mysynthesis_ecodriving_level_very_bad
-                DKScoreType.DISTRACTION -> R.string.dk_driverdata_mysynthesis_distraction_level_very_bad
-                DKScoreType.SPEEDING -> R.string.dk_driverdata_mysynthesis_speeding_level_very_bad
-            }
-        }
 
     private fun tagScreen() {
         DriveKitUI.analyticsListener?.trackScreen(
