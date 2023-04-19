4250329493f9bc1519cfbd3cbf3a3b1205e1103d
David Bauduin <david.bauduin@drivequant.com>
2023-04-19 20:59:22 +0800

    Improve UI of add vehicle screen depending on configuration type

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/picker/fragments/VehicleCategoryDescriptionFragment.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/picker/fragments/VehicleCategoryDescriptionFragment.kt
@@ -8,12 +8,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.StringRes
 import com.drivequant.drivekit.common.ui.DriveKitUI
-import com.drivequant.drivekit.common.ui.extension.button
-import com.drivequant.drivekit.common.ui.extension.headLine2
-import com.drivequant.drivekit.common.ui.extension.normalText
-import com.drivequant.drivekit.common.ui.extension.setDKStyle
-import com.drivequant.drivekit.common.ui.utils.DKResource
+import com.drivequant.drivekit.common.ui.extension.*
 import com.drivequant.drivekit.vehicle.ui.DriveKitVehicleUI
 import com.drivequant.drivekit.vehicle.ui.R
 import com.drivequant.drivekit.vehicle.ui.picker.commons.VehiclePickerStep
@@ -40,38 +37,55 @@ class VehicleCategoryDescriptionFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_vehicle_category_description, container,false).setDKStyle()
+    ): View = if (DriveKitVehicleUI.categoryConfigType == CategoryConfigType.LITE_CONFIG_ONLY) {
+        R.layout.fragment_vehicle_category_description_lite_config
+    } else {
+        R.layout.fragment_vehicle_category_description
+    }.let {
+        inflater.inflate(it, container, false).setDKStyle()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val imageViewCategory = view.findViewById(R.id.image_view_icon2) as ImageView
         val textViewDescription = view.findViewById(R.id.text_view_description) as TextView
-        val buttonValidate = view.findViewById(R.id.button_validate) as Button
-        val textViewBrands = view.findViewById(R.id.text_view_brands) as TextView
+        val validateButton = view.findViewById(R.id.button_validate) as View
+        val textViewBrands = view.findViewById(R.id.text_view_brands) as View
 
-        buttonValidate.button()
-        buttonValidate.text = DKResource.convertToString(requireContext(), "dk_common_validate")
-        buttonValidate.setOnClickListener {
+        if (DriveKitVehicleUI.categoryConfigType == CategoryConfigType.LITE_CONFIG_ONLY) {
+            val buttonValidate = validateButton as Button
+            buttonValidate.button()
+            buttonValidate.setText(R.string.dk_common_validate)
+            textViewBrands.visibility = View.GONE
+        } else {
+            customizeButton(textViewBrands, R.string.dk_vehicle_detail_category_button_title, R.string.dk_vehicle_detail_category_button_description)
+            customizeButton(validateButton, R.string.dk_vehicle_quick_category_button_title, R.string.dk_vehicle_quick_category_button_description)
+            textViewBrands.setOnClickListener {
+                viewModel.computeNextScreen(requireContext(), VehiclePickerStep.CATEGORY_DESCRIPTION, otherAction = true)
+            }
+        }
+        validateButton.setOnClickListener {
             viewModel.computeNextScreen(requireContext(), VehiclePickerStep.CATEGORY_DESCRIPTION)
         }
-
-        textViewBrands.headLine2(DriveKitUI.colors.secondaryColor())
-        textViewBrands.text = DKResource.convertToString(requireContext(), "dk_vehicle_category_display_brands")
 
         if (this::vehiclePickerCategoryItem.isInitialized) {
             textViewDescription.normalText()
             textViewDescription.text = vehiclePickerCategoryItem.description
             imageViewCategory.setImageDrawable(vehiclePickerCategoryItem.icon2)
         }
-        
-        if (DriveKitVehicleUI.categoryConfigType != CategoryConfigType.LITE_CONFIG_ONLY) {
-            textViewBrands.visibility = View.VISIBLE
-            textViewBrands.setOnClickListener {
-                viewModel.computeNextScreen(requireContext(), VehiclePickerStep.CATEGORY_DESCRIPTION, otherAction = true)
-            }
-        } else {
-            textViewBrands.visibility = View.GONE
+    }
+
+    private fun customizeButton(button: View, @StringRes titleId: Int, @StringRes descriptionId: Int) {
+        button.background.tintDrawable(DriveKitUI.colors.secondaryColor())
+        button.findViewById<TextView>(R.id.buttonTitle).run {
+            isAllCaps = true
+            headLine2(DriveKitUI.colors.secondaryColor())
+            setText(titleId)
+        }
+        button.findViewById<TextView>(R.id.buttonDescription).run {
+            smallText(DriveKitUI.colors.complementaryFontColor())
+            setText(descriptionId)
         }
     }
 }
