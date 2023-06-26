357dd360fa430495a21914995928c52bfec57e91
David Bauduin <david.bauduin@drivequant.com>
2023-06-26 23:24:46 +0800

    Sync timeline in DriverProfile

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/driverprofile/DriverProfileViewModel.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/driverprofile/DriverProfileViewModel.kt
@@ -6,21 +6,30 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.drivequant.drivekit.core.SynchronizationType
+import com.drivequant.drivekit.databaseutils.entity.DKPeriod
 import com.drivequant.drivekit.driverdata.DriveKitDriverData
 import com.drivequant.drivekit.driverdata.driverprofile.DKDriverProfile
 import com.drivequant.drivekit.driverdata.driverprofile.DKDriverProfileStatus
+import com.drivequant.drivekit.driverdata.timeline.DKDriverTimeline
+import com.drivequant.drivekit.driverdata.timeline.TimelineSyncStatus
 
 internal class DriverProfileViewModel(application: Application) : AndroidViewModel(application) {
 
-    var driverProfile: DKDriverProfile? = null
-        private set
     val dataUpdated = MutableLiveData<DataState>()
+    private var driverProfile: DKDriverProfile? = null
+    private var currentDrivenDistanceByPeriod: Map<DKPeriod, Double> = mapOf()
+    private val timelinePeriods = listOf(DKPeriod.WEEK, DKPeriod.MONTH, DKPeriod.YEAR)
 
     init {
         DriveKitDriverData.getDriverProfile(SynchronizationType.CACHE) { status, driverProfile ->
             if (status == DKDriverProfileStatus.SUCCESS) {
                 this.driverProfile = driverProfile
-                this.dataUpdated.postValue(DataState.UPDATED)
+                DriveKitDriverData.getDriverTimelines(timelinePeriods, SynchronizationType.CACHE) { timelineStatus, timelines ->
+                    if (timelineStatus == TimelineSyncStatus.CACHE_DATA_ONLY) {
+                        updateDrivenDistances(timelines)
+                    }
+                    this.dataUpdated.postValue(DataState.UPDATED)
+                }
             } else {
                 this.dataUpdated.postValue(DataState.NO_DATA_YET)
             }
@@ -30,16 +39,24 @@ internal class DriverProfileViewModel(application: Application) : AndroidViewMod
 
     fun updateData() {
         DriveKitDriverData.getDriverProfile(SynchronizationType.DEFAULT) { status, driverProfile ->
-            val dataState: DataState = when (status) {
-                DKDriverProfileStatus.NO_DRIVER_PROFILE_YET -> DataState.NO_DATA_YET
-                DKDriverProfileStatus.FORBIDDEN_ACCESS -> DataState.FORBIDDEN
+            when (status) {
+                DKDriverProfileStatus.NO_DRIVER_PROFILE_YET -> this.dataUpdated.postValue(DataState.NO_DATA_YET)
+                DKDriverProfileStatus.FORBIDDEN_ACCESS -> this.dataUpdated.postValue(DataState.FORBIDDEN)
                 DKDriverProfileStatus.SUCCESS, DKDriverProfileStatus.FAILED_TO_SYNC_DRIVER_PROFILE_CACHE_ONLY -> {
                     this.driverProfile = driverProfile
-                    DataState.UPDATED
+                    DriveKitDriverData.getDriverTimelines(timelinePeriods, SynchronizationType.DEFAULT) { timelineStatus, timelines ->
+                        if (timelineStatus == TimelineSyncStatus.NO_ERROR) {
+                            updateDrivenDistances(timelines)
+                        }
+                        this.dataUpdated.postValue(DataState.UPDATED)
+                    }
                 }
             }
-            this.dataUpdated.postValue(dataState)
         }
+    }
+
+    private fun updateDrivenDistances(timelines: List<DKDriverTimeline>) {
+        this.currentDrivenDistanceByPeriod = timelines.associateBy({ it.period }, { it.allContext.lastOrNull()?.distance ?: 0.0 })
     }
 
     class DriverProfileViewModelFactory(private val application: Application) :
