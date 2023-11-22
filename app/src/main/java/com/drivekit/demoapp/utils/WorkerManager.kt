d0cc67239ad8df949c86c6c5f3df70b85b429bd7
Steven <steven@drivequant.com>
2023-11-22 18:58:55 +0100

    Rename enum

--- app/src/main/java/com/drivekit/demoapp/utils/WorkerManager.kt
+++ app/src/main/java/com/drivekit/demoapp/utils/WorkerManager.kt
@@ -9,16 +9,16 @@ import java.util.concurrent.TimeUnit
 
 internal object WorkerManager {
     private enum class WorkerTag {
-        APP_DIAGNOSIS
+        DIAGNOSIS_NOTIFICATION
     }
 
     fun startAppDiagnosisWorker(context: Context) {
         val worker = PeriodicWorkRequestBuilder<DiagnosisNotificationWorker>(2, TimeUnit.HOURS).build()
-        WorkManager.getInstance(context).enqueueUniquePeriodicWork(WorkerTag.APP_DIAGNOSIS.name, ExistingPeriodicWorkPolicy.KEEP, worker)
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(WorkerTag.DIAGNOSIS_NOTIFICATION.name, ExistingPeriodicWorkPolicy.KEEP, worker)
     }
 
     fun stopAppDiagnosisWorker(context: Context) {
-        WorkManager.getInstance(context).cancelAllWorkByTag(WorkerTag.APP_DIAGNOSIS.name)
+        WorkManager.getInstance(context).cancelAllWorkByTag(WorkerTag.DIAGNOSIS_NOTIFICATION.name)
     }
 
     fun reset(context: Context) {
