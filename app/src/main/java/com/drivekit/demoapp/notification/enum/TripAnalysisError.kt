2cba2e342fb7bf8d4704a89c772587e0736d031f
David Bauduin <david.bauduin@drivequant.com>
2024-01-04 18:02:29 +0100

    Manage more error to display in the trip analysis finished notification (#349)
    * Manage more error to display in the trip analysis finished notification
    
    * Remove unused method
    
    * Add missing log
    
    * Fix one string
    
    * Remove useless code

--- app/src/main/java/com/drivekit/demoapp/notification/enum/TripAnalysisError.kt
+++
@@ -1,9 +0,0 @@
-package com.drivekit.demoapp.notification.enum
-
-internal enum class TripAnalysisError {
-    DUPLICATE_TRIP,
-    NO_NETWORK,
-    NO_API_KEY,
-    NO_BEACON,
-    NO_BLUETOOTH_DEVICE
-}
---
+++ app/src/main/java/com/drivekit/demoapp/notification/enum/TripResponseErrorNotification.kt
@@ -0,0 +1,92 @@
+package com.drivekit.demoapp.notification.enum
+
+import androidx.annotation.StringRes
+import com.drivekit.tripanalysis.ui.R
+import com.drivequant.drivekit.tripanalysis.entity.TripResponseError
+
+internal enum class TripResponseErrorNotification(@StringRes val descriptionKey: Int) {
+    INVALID_ROUTE_DEFINITION(R.string.dk_trip_service_insufficient_gps_data),
+    INVALID_SAMPLING_PERIOD(R.string.dk_trip_service_invalid_sample_period),
+    INVALID_CUSTOMER_ID(R.string.dk_trip_service_error_invalid_customer_id),
+    MAX_DAILY_REQUEST_NUMBER_REACHED(R.string.dk_trip_service_request_limit_reached),
+    DATA_ERROR(R.string.dk_trip_service_analysis_failed),
+    INVALID_ROUTE_VECTORS(R.string.dk_trip_service_analysis_failed),
+    DUPLICATE_TRIP(R.string.dk_trip_service_error_trip_duplicate),
+    INSUFFICIENT_GPS_DATA(R.string.dk_trip_service_insufficient_gps_data),
+    USER_DISABLED(R.string.dk_trip_service_user_disabled),
+    INVALID_USER(R.string.dk_trip_service_user_invalid),
+    INVALID_GPS_DATA(R.string.dk_trip_service_invalid_gps_data),
+    INVALID_TRIP(R.string.dk_trip_service_invalid_gps_data),
+    ACCOUNT_LIMIT_REACHED(R.string.dk_trip_service_account_limit_reached),
+    MISSING_BEACON(R.string.dk_trip_service_error_beacon_missing),
+    INVALID_BEACON(R.string.dk_trip_service_error_beacon_invalid);
+
+    companion object {
+        fun fromTripResponseError(tripResponseError: TripResponseError): TripResponseErrorNotification? =
+            when (tripResponseError) {
+                TripResponseError.INVALID_ROUTE_DEFINITION -> INVALID_ROUTE_DEFINITION
+                TripResponseError.INVALID_SAMPLING_PERIOD -> INVALID_SAMPLING_PERIOD
+                TripResponseError.INVALID_CUSTOMER_ID -> INVALID_CUSTOMER_ID
+                TripResponseError.MAX_DAILY_REQUEST_NUMBER_REACHED -> MAX_DAILY_REQUEST_NUMBER_REACHED
+                TripResponseError.DATA_ERROR -> DATA_ERROR
+                TripResponseError.INVALID_ROUTE_VECTORS -> INVALID_ROUTE_VECTORS
+                TripResponseError.DUPLICATE_TRIP -> DUPLICATE_TRIP
+                TripResponseError.INSUFFICIENT_GPS_DATA -> INSUFFICIENT_GPS_DATA
+                TripResponseError.USER_DISABLED -> USER_DISABLED
+                TripResponseError.INVALID_USER -> INVALID_USER
+                TripResponseError.INVALID_GPS_DATA -> INVALID_GPS_DATA
+                TripResponseError.INVALID_TRIP -> INVALID_TRIP
+                TripResponseError.ACCOUNT_LIMIT_REACHED -> ACCOUNT_LIMIT_REACHED
+                TripResponseError.MISSING_BEACON -> MISSING_BEACON
+                TripResponseError.INVALID_BEACON -> INVALID_BEACON
+
+                TripResponseError.NO_ACCOUNT_SET,
+                TripResponseError.NO_ROUTE_OBJECT_FOUND,
+                TripResponseError.NO_VELOCITY_DATA,
+                TripResponseError.NO_DATE_FOUND,
+                TripResponseError.UNKNOWN_ERROR -> null
+            }
+    }
+
+    fun getNotificationChannel(): DKNotificationChannel =
+        when (this) {
+            INVALID_ROUTE_DEFINITION,
+            INVALID_SAMPLING_PERIOD,
+            INVALID_CUSTOMER_ID,
+            MAX_DAILY_REQUEST_NUMBER_REACHED,
+            DATA_ERROR,
+            INVALID_ROUTE_VECTORS,
+            DUPLICATE_TRIP,
+            INSUFFICIENT_GPS_DATA,
+            USER_DISABLED,
+            INVALID_USER,
+            INVALID_GPS_DATA,
+            INVALID_TRIP,
+            ACCOUNT_LIMIT_REACHED -> DKNotificationChannel.TRIP_ENDED
+
+            MISSING_BEACON,
+            INVALID_BEACON -> DKNotificationChannel.TRIP_CANCELLED
+        }
+
+    fun getNotificationId() =
+        when (this) {
+            INVALID_ROUTE_DEFINITION -> 208
+            INVALID_SAMPLING_PERIOD -> 209
+            INVALID_CUSTOMER_ID -> 204
+            MAX_DAILY_REQUEST_NUMBER_REACHED -> 210
+
+            DATA_ERROR,
+            INVALID_ROUTE_VECTORS -> 211
+
+            DUPLICATE_TRIP -> 203
+            INSUFFICIENT_GPS_DATA -> 212
+            USER_DISABLED -> 213
+            INVALID_USER -> 214
+            INVALID_GPS_DATA -> 215
+            INVALID_TRIP -> 216
+            ACCOUNT_LIMIT_REACHED -> 217
+
+            MISSING_BEACON,
+            INVALID_BEACON -> 252
+        }
+}
