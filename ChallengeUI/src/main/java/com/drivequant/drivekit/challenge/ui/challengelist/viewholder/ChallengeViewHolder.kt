6802fc9aa0f4f59642fb0727c1a236845ea4a0c2
Steven <56969493+Steven-DriveQuant@users.noreply.github.com>
2024-01-30 11:44:23 +0100

    Feature/560 update challenge list (#357)
    * WIP Start to rework challenge list
    
    * Start to display challenge list by category and by year
    
    * Display challenge list by category and by year
    
    * Update DriveKit modules, manage empty text in challenge list
    
    * Manage participation text
    
    * Adjust UI
    
    * Finish feature
    
    * Restore elevation for challenge list item CardView
    
    * In challenge list, always display nbRegistered instead of nbRanked
    
    * Display loader when entering on the challenge list screen
    
    * Improve Challenge list adapter
    
    * Clean unused resources
    
    * Replace ChallengeState by isFinished property
    
    * Fix indentation
    
    * Rename incorrect id
    
    * Apply changes after review
    
    * Remove redundant useless toSpannable()
    
    * Update condition for displaying challenge detail
    
    * Apply changes after review
    
    * Clean code and scroll to the first item when the challenge list is updated
    
    * Update strings

--- ChallengeUI/src/main/java/com/drivequant/drivekit/challenge/ui/challengelist/viewholder/ChallengeViewHolder.kt
+++ ChallengeUI/src/main/java/com/drivequant/drivekit/challenge/ui/challengelist/viewholder/ChallengeViewHolder.kt
@@ -8,24 +8,37 @@ import com.drivequant.drivekit.challenge.ui.R
 import com.drivequant.drivekit.challenge.ui.challengelist.viewmodel.ChallengeData
 import com.drivequant.drivekit.common.ui.DriveKitUI
 import com.drivequant.drivekit.common.ui.extension.formatDate
-import com.drivequant.drivekit.common.ui.extension.highlightSmall
+import com.drivequant.drivekit.common.ui.extension.headLine2
 import com.drivequant.drivekit.common.ui.extension.normalText
+import com.drivequant.drivekit.common.ui.extension.smallText
+import com.drivequant.drivekit.common.ui.extension.tintDrawable
 import com.drivequant.drivekit.common.ui.utils.DKDatePattern
 
 internal class ChallengeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val challengeTitle = itemView.findViewById<TextView>(R.id.dk_text_view_title)
-    private val challengeDate = itemView.findViewById<TextView>(R.id.dk_text_view_date)
     private val challengeIcon = itemView.findViewById<ImageView>(R.id.dk_image_view_icon)
+    private val calendarIcon = itemView.findViewById<ImageView>(R.id.calendar_icon)
+    private val title = itemView.findViewById<TextView>(R.id.dk_text_view_title)
+    private val dates = itemView.findViewById<TextView>(R.id.dk_text_view_date)
+    private val participationText = itemView.findViewById<TextView>(R.id.dk_text_view_participation)
 
-    fun bind(challengeListData: ChallengeData) {
-        challengeTitle.text = challengeListData.title
-        challengeDate.text = "${challengeListData.startDate.formatDate(DKDatePattern.STANDARD_DATE)} - ${challengeListData.endDate.formatDate(DKDatePattern.STANDARD_DATE)}"
-        challengeIcon.setImageResource(challengeListData.getChallengeResourceId())
+    fun bind(challengeData: ChallengeData) {
+        challengeData.getIconResId()?.let {
+            challengeIcon.setImageResource(it)
+        }
+        title.text = challengeData.title
+        dates.text = buildString {
+            append(challengeData.startDate.formatDate(DKDatePattern.STANDARD_DATE))
+            append(" - ")
+            append(challengeData.endDate.formatDate(DKDatePattern.STANDARD_DATE))
+        }
+        participationText.text = challengeData.getParticipationText(itemView.context)
         setStyle()
     }
 
     private fun setStyle() {
-        challengeTitle.highlightSmall()
-        challengeDate.normalText(DriveKitUI.colors.complementaryFontColor())
+        this.title.headLine2(DriveKitUI.colors.primaryColor())
+        this.calendarIcon.drawable.tintDrawable(DriveKitUI.colors.complementaryFontColor())
+        this.dates.smallText(DriveKitUI.colors.complementaryFontColor())
+        this.participationText.normalText(DriveKitUI.colors.secondaryColor())
     }
 }
