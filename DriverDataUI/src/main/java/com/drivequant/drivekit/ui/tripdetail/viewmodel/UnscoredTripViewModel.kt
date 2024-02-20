2b8887fb7c18d10b5a4411e7a9fd7c5447c97d9f
Steven <steven@drivequant.com>
2024-02-20 10:05:26 +0100

    Manage round corners for unscored trip card

--- DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/viewmodel/UnscoredTripViewModel.kt
+++ DriverDataUI/src/main/java/com/drivequant/drivekit/ui/tripdetail/viewmodel/UnscoredTripViewModel.kt
@@ -1,7 +1,6 @@
 package com.drivequant.drivekit.ui.tripdetail.viewmodel
 
 import com.drivequant.drivekit.databaseutils.entity.Trip
-import com.drivequant.drivekit.ui.R
 import com.drivequant.drivekit.ui.extension.getOrComputeStartDate
 import java.io.Serializable
 import java.util.*
@@ -14,6 +13,4 @@ class UnscoredTripViewModel(
     fun getStartDate(): Date? = trip?.getOrComputeStartDate()
 
     fun getEndDate(): Date? = trip?.endDate
-
-    fun getNoScoreTripMessage(): Int = R.string.dk_driverdata_trip_detail_no_score
 }
