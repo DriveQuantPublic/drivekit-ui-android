6de3bafebf5a6c41f0fd3b6df3bc98c3dd156c63
David Bauduin <david.bauduin@drivequant.com>
2024-06-10 09:50:40 +0200

    Remove useless code

--- CommonUI/src/main/java/com/drivequant/drivekit/common/ui/DriveKitUI.kt
+++ CommonUI/src/main/java/com/drivequant/drivekit/common/ui/DriveKitUI.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Typeface
 import androidx.core.content.res.ResourcesCompat
 import com.drivequant.drivekit.common.ui.analytics.DriveKitAnalyticsListener
-import com.drivequant.drivekit.common.ui.graphical.DKFonts
 import com.drivequant.drivekit.common.ui.utils.DistanceUnit
 import com.drivequant.drivekit.core.DriveKit
 import com.drivequant.drivekit.core.DriveKitLog
@@ -28,9 +27,6 @@ object DriveKitUI {
             }
         }
 
-    @JvmStatic
-    val fonts = DKFonts()
-
     init {
         DriveKit.checkInitialization()
         DriveKitLog.i(TAG, "Initialization")
@@ -47,8 +43,8 @@ object DriveKitUI {
     }
 
     @JvmStatic
-    fun primaryFont(context: Context): Typeface? = ResourcesCompat.getFont(context, fonts.primaryFont())
+    fun primaryFont(context: Context): Typeface? = ResourcesCompat.getFont(context, R.font.dkprimary)
 
     @JvmStatic
-    fun secondaryFont(context: Context): Typeface? = ResourcesCompat.getFont(context, fonts.secondaryFont())
+    fun secondaryFont(context: Context): Typeface? = ResourcesCompat.getFont(context, R.font.dksecondary)
 }
