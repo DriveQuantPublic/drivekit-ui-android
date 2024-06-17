5eae21b25f15d31f1d7f6e85da6fcdd2d0ee8ac5
Steven <steven@drivequant.com>
2024-06-17 12:07:24 +0200

    Manage custom image rotation by reading ExifInformation data

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/DriveKitVehicleUI.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/DriveKitVehicleUI.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.util.Log
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.drivequant.drivekit.common.ui.adapter.FilterItem
 import com.drivequant.drivekit.common.ui.listener.ContentMail
--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/utils/VehicleUtils.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/utils/VehicleUtils.kt
@@ -3,13 +3,15 @@ package com.drivequant.drivekit.vehicle.ui.vehicles.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
 import android.text.TextUtils
 import androidx.annotation.DrawableRes
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.exifinterface.media.ExifInterface
 import com.drivequant.drivekit.core.DriveKitSharedPreferencesUtils
 import com.drivequant.drivekit.databaseutils.Query
 import com.drivequant.drivekit.databaseutils.entity.Vehicle
@@ -18,7 +20,6 @@ import com.drivequant.drivekit.vehicle.ui.R
 import com.drivequant.drivekit.vehicle.ui.extension.buildFormattedName
 import com.drivequant.drivekit.vehicle.ui.extension.getImageByTypeIndex
 import java.io.FileNotFoundException
-
 
 object VehicleUtils {
 
@@ -86,18 +87,47 @@ object VehicleUtils {
         val defaultVehicleDrawable = ResourcesCompat.getDrawable(context.resources, getFilterVehicleDrawable(vehicleId), null)
 
         val imageUri = DriveKitSharedPreferencesUtils.getString(String.format("drivekit-vehicle-picture_%s", vehicleId))
-        return if (!TextUtils.isEmpty(imageUri)) {
+        if (!TextUtils.isEmpty(imageUri)) {
             try {
                 val uri = Uri.parse(imageUri)
                 val stream = context.contentResolver.openInputStream(uri)
                 val b = BitmapFactory.decodeStream(stream)
                 b.density = Bitmap.DENSITY_NONE
-                BitmapDrawable(context.resources, b)
+
+                val orientation = getImageOrientation(context, uri)
+                if (orientation != null) {
+                    val matrix = Matrix()
+                    matrix.setRotate(orientation.toFloat())
+                    val bitmap = Bitmap.createBitmap(b, 0, 0, b.width, b.height, matrix, true)
+                    return BitmapDrawable(context.resources, bitmap)
+                }
+                return BitmapDrawable(context.resources, b)
             } catch (e: FileNotFoundException){
-                defaultVehicleDrawable
+                return defaultVehicleDrawable
             }
         } else {
-            defaultVehicleDrawable
+            return defaultVehicleDrawable
         }
+    }
+
+    private fun getImageOrientation(context: Context, uri: Uri): Int? {
+        val stream = context.contentResolver.openInputStream(uri)
+
+        val exifInterface = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && stream != null) {
+            ExifInterface(stream)
+        } else {
+            null // DriveKit will no longer support Android 6.0 by the end of 2024.
+        }
+
+        exifInterface?.let {
+            val orientation = it.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            return when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_270 -> 270
+                ExifInterface.ORIENTATION_ROTATE_180 -> 180
+                ExifInterface.ORIENTATION_ROTATE_90 -> 90
+                else -> null
+            }
+        }
+        return null
     }
 }
