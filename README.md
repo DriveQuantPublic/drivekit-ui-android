02cac18d8c37018b8300cd6b7b7cf219ad4faff9
Steven <steven@drivequant.com>
2024-12-12 17:15:14 +0100

    Update README and remove unused imports

--- README.md
+++ README.md
@@ -56,8 +56,8 @@ For an integration on Android you will need the following elements:
 
 Your project must be configured as follows: 
 * `minSdkVersion = 23`
-* `compileSdkVersion = 34`
-* `targetSdkVersion = 34`
+* `compileSdkVersion = 35`
+* `targetSdkVersion = 35`
 
 The DriveKit SDK uses the libraries listed below. These are minimal required versions. Check that they are compatible with your application.
 * `Kotlin = 1.8.22`
--- TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/triprecordingwidget/recordingbutton/DKTripRecordingButtonViewModel.kt
+++ TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/triprecordingwidget/recordingbutton/DKTripRecordingButtonViewModel.kt
@@ -14,8 +14,6 @@ import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysis
 import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysisUI
 import com.drivequant.drivekit.tripanalysis.TripListener
 import com.drivequant.drivekit.tripanalysis.entity.TripPoint
-import com.drivequant.drivekit.tripanalysis.service.recorder.CancelTrip
-import com.drivequant.drivekit.tripanalysis.service.recorder.StartMode
 import com.drivequant.drivekit.tripanalysis.service.recorder.State
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
