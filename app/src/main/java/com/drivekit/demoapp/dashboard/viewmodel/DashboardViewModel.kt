14915b38c260fb35175f6881af77d77b91962b38
Steven <steven@drivequant.com>
2025-02-27 14:22:43 +0100

    Hide simulation button if it's the noop module

--- app/src/main/java/com/drivekit/demoapp/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/drivekit/demoapp/dashboard/viewmodel/DashboardViewModel.kt
@@ -3,12 +3,16 @@ package com.drivekit.demoapp.dashboard.viewmodel
 import androidx.lifecycle.ViewModel
 import com.drivekit.demoapp.features.enum.FeatureType
 import com.drivequant.drivekit.common.ui.component.triplist.viewModel.HeaderDay
+import com.drivequant.drivekit.tripsimulator.DriveKitTripSimulator
 import com.drivequant.drivekit.ui.DriverDataUI
 import com.drivequant.drivekit.ui.SynthesisCardsViewListener
 import com.drivequant.drivekit.ui.synthesiscards.LastTripsSynthesisCard
 import com.drivequant.drivekit.ui.synthesiscards.fragment.DKSynthesisCardViewPagerFragment
 
 internal class DashboardViewModel: ViewModel() {
+
+    val displayTripSimulator: Boolean = !DriveKitTripSimulator.isNoop
+
     fun getSynthesisCardsView(listener: SynthesisCardsViewListener) {
         DriverDataUI.getLastTripsSynthesisCardsView(
             listOf(
