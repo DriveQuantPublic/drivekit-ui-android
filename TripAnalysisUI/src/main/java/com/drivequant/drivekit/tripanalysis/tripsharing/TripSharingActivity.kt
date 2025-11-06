e3600943c96f28d5d6e8496ff2e8fd4a23121a96
Steven <steven@drivequant.com>
2025-11-06 19:14:08 +0100

    chore: remove unused imports

--- TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/tripsharing/TripSharingActivity.kt
+++ TripAnalysisUI/src/main/java/com/drivequant/drivekit/tripanalysis/tripsharing/TripSharingActivity.kt
@@ -18,13 +18,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
