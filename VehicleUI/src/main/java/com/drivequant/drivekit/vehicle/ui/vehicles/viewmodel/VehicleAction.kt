7904014424e5aab43b178ee0c2db0bfb4bdb8ea6
dq-guillaumegremillet <guillaume.gremillet@drivequant.com>
2025-12-12 15:22:34 +0100

    fix: check for last location of a vehicle to show the option
    Co-authored-by: Steven <56969493+Steven-DriveQuant@users.noreply.github.com>

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/viewmodel/VehicleAction.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/vehicles/viewmodel/VehicleAction.kt
@@ -42,7 +42,7 @@ enum class VehicleAction(
             REPLACE -> true
             DELETE -> DriveKitVehicle.vehiclesQuery().noFilter().countQuery().execute() > 1
             ODOMETER -> DriveKitVehicleUI.hasOdometer
-            FIND_MY_VEHICLE -> DriveKitVehicle.getVehicleLocation(vehicle.vehicleId) !== null
+            FIND_MY_VEHICLE -> DriveKitVehicle.getVehicleLocation(vehicle.vehicleId) != null
         }
     }
 
