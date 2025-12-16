9e9b9fe5c867edaf43bed79fddf5e9a51afe5530
Steven <steven@drivequant.com>
2025-12-16 17:11:36 +0100

    feat: vehicle picker - replace Views button for "default car engine"

--- VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/picker/fragments/DefaultCarEngineSelectionFragment.kt
+++ VehicleUI/src/main/java/com/drivequant/drivekit/vehicle/ui/picker/fragments/DefaultCarEngineSelectionFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.drivequant.drivekit.common.ui.component.DKPrimaryButton
 import com.drivequant.drivekit.common.ui.extension.normalText
 import com.drivequant.drivekit.common.ui.extension.setDKStyle
 import com.drivequant.drivekit.vehicle.ui.R
@@ -38,14 +39,17 @@ internal class DefaultCarEngineSelectionFragment : Fragment() {
         val descriptionTextView: TextView = view.findViewById(R.id.text_view_description)
         val yesChoice: RadioButton = view.findViewById(R.id.choiceYes)
         val noChoice: RadioButton = view.findViewById(R.id.choiceNo)
-        val validateButton: Button = view.findViewById(R.id.button_validate)
+        val validateButton: ComposeView = view.findViewById(R.id.button_validate)
 
         descriptionTextView.normalText()
         yesChoice.normalText()
         noChoice.normalText()
-        validateButton.setOnClickListener {
-            viewModel.isElectric = yesChoice.isChecked
-            viewModel.computeNextScreen(requireContext(), VehiclePickerStep.DEFAULT_CAR_ENGINE)
+
+        validateButton.setContent {
+            DKPrimaryButton(getString(com.drivequant.drivekit.common.ui.R.string.dk_common_validate)) {
+                viewModel.isElectric = yesChoice.isChecked
+                viewModel.computeNextScreen(requireContext(), VehiclePickerStep.DEFAULT_CAR_ENGINE)
+            }
         }
     }
 }
